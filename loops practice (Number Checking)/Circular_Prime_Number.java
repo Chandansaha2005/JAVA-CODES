951cc2effe02dc41e2b82085ffe4a428b1c17216
Chandan Saha <chandansaha1945@gmail.com>
2025-06-13 16:22:46 +0530

    Twin prime number

---
+++ loops practice (Number Checking)/Circular_Prime_Number.java
@@ -0,0 +1,13 @@
+
+// Circular Prime Number
+// Definition: All rotations of the digits are prime.
+// Example: 197 → 197, 971, 719 → All prime → Circular Prime
+import java.util.Scanner;
+
+public class Circular_Prime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        
+    }
+}
---
+++ loops practice (Number Checking)/Twin_Prime_Number.java
@@ -0,0 +1,35 @@
+
+// Twin Prime Number
+// Definition: Prime numbers differing by 2.
+// Example: (11,13), (17,19) → Twin Primes
+import java.util.Scanner;
+
+public class Twin_Prime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The 1st Number = ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter The 2nd Number = ");
+        int num2 = sc.nextInt();
+        if (isPrime(num1) && isPrime(num2)) {
+            if (num1 + 2 == num2 || num2 + 2 == num1) {
+                System.out.println("(" + num1 + "," + num2 + ") are Twin Prime no.");
+            } else {
+                System.out.println("(" + num1 + "," + num2 + ")Both are Prime But Not Twin Prime no.");
+            }
+        } else {
+            System.out.println("(" + num1 + "," + num2 + ")Both are Not Prime no.");
+        }
+
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
