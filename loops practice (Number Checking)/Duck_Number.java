6037401c4da4071a9634993d6b27c83452005dd2
Chandan Saha <chandansaha1945@gmail.com>
2025-06-13 12:15:55 +0530

    duck number

--- loops practice (Number Checking)/Duck_Number.java
+++ loops practice (Number Checking)/Duck_Number.java
@@ -9,8 +9,23 @@ public class Duck_Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number = ");
-        int num=sc.nextInt();
-        
+        String num = sc.next();
+        boolean flag = true;
+        if (num.charAt(0) == '0') {
+            flag = false;
+        } else {
+            for (int i =1; i < num.length(); i++) {
+                if (num.charAt(i) == '0') {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            System.out.println(num + " is a duck number");
+        } else {
+            System.out.println(num + " is Not a duck number");
+        }
         sc.close();
     }
 }
