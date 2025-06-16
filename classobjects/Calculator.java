3a8499b8f4bd37b93b9657a84485c74d5d175856
Chandan Saha <chandansaha1945@gmail.com>
2025-06-16 23:36:33 +0530

    creating a simple calculator using classes and objects

---
+++ classobjects/Calculator.java
@@ -0,0 +1,31 @@
+class Add{
+    int a;
+    int b;
+    int Addition(int a , int b ){
+        return a+b;
+    }
+}
+class sub{
+    int a;
+    int b;
+    int Subtraction(int a , int b ){
+        return a-b;
+    }
+}
+class mul{
+    int a;
+    int b;
+    int Multiplecation(int a , int b ){
+        return a*b;
+    }
+}
+class div{
+    int a;
+    int b;
+    int Divition(int a , int b ){
+        return a/b;
+    }
+}
+public class Calculator {
+    
+}
