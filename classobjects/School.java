08cb0a429160fa47639e33077ccd316497ede313
Chandan Saha <chandansaha1945@gmail.com>
2025-06-16 23:21:20 +0530

    we can also give input to an attribute of another class

--- classobjects/School.java
+++ classobjects/School.java
@@ -1,10 +1,13 @@
-class Student {
-    int RollNo = 10;
+class Student { // creating a new class named Student
+    int RollNo = 10; // giving an attribute
+    String name;
 }
 
-public class School {
+public class School { // this public class must have the same name as the file
     public static void main(String[] args) {
-        Student ob = new Student();
-        System.out.println(ob.RollNo);
+        Student ob = new Student(); // creating an object of Student class
+        System.out.println(ob.RollNo); // using the object to access the attribute
+        ob.name = "chandan";
+        System.out.println(ob.name); // using the object to access the attribute
     }
 }
