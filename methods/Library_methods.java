e3ca99e08e816acf97ac72c3fafa9b0fd234ea4e
Chandan Saha <chandansaha1945@gmail.com>
2025-06-17 23:19:31 +0530

    standard library methods

---
+++ methods/Library_methods.java
@@ -0,0 +1,12 @@
+public class Library_methods {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.sqrt(16));
+
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(5.7));
+
+        System.out.println(Math.ceil(6.9));
+        System.out.println(Math.ceil(5.5));
+    }
+}
