7e5834dab1ca4c107304d80b918fc6c9e0a3229b
Chandan Saha <chandansaha1945@gmail.com>
2025-06-17 23:55:59 +0530

    Packages

---
+++ methods/SourcePack/example/App.java
@@ -0,0 +1,9 @@
+package SourcePack.example;
+
+public class App {
+
+}
+
+class App2 {
+
+}
---
+++ methods/TryPackage.java
@@ -0,0 +1,7 @@
+import SourcePack.example.App;
+
+public class TryPackage {
+    public static void main(String[] args) {
+        App ob = new App();
+    }
+}
