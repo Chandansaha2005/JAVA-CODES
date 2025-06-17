213dfd492195dd7e4fd95820f71d0934e266010a
Chandan Saha <chandansaha1945@gmail.com>
2025-06-17 12:12:22 +0530

    method file added

---
+++ methods/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static void HelloWorld() {// creating a method,the return type is void
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld();// calling the method
+    }
+}
