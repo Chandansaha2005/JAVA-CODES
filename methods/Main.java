7271d719b7f9fa4c026a1975d8642d6a71b6e94f
Chandan Saha <chandansaha1945@gmail.com>
2025-06-17 22:37:56 +0530

    static

--- methods/Main.java
+++ methods/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     static void HelloWorld() {// creating a method,the return type is void
+        // for the "static" modifier we can access the function with out creating any
+        // object
         System.out.println("Hello World");
     }
 
     public static void main(String[] args) {
         HelloWorld();// calling the method
+
     }
 }
