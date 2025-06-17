0c7d6a72ef29a6144851dc6cb443701bb02d64e3
Chandan Saha <chandansaha1945@gmail.com>
2025-06-17 23:21:18 +0530

    update

--- methods/Library_methods.java
+++ methods/Library_methods.java
@@ -3,10 +3,10 @@ public class Library_methods {
         System.out.println(Math.sqrt(24));
         System.out.println(Math.sqrt(16));
 
-        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(5.3));// Gives the Round of this number (-)
         System.out.println(Math.floor(5.7));
 
-        System.out.println(Math.ceil(6.9));
+        System.out.println(Math.ceil(6.9));// Gives the Round of this number (+)
         System.out.println(Math.ceil(5.5));
     }
 }
