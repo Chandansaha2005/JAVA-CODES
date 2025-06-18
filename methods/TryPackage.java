d9902e18e4f7dceeaf43b366eba057df7e82aa7e
Chandan Saha <chandansaha1945@gmail.com>
2025-06-18 13:50:49 +0530

    package done

--- methods/TryPackage.java
+++ methods/TryPackage.java
@@ -1,7 +1,13 @@
-import SourcePack.example.App;
+// Importing the App class from the package SourcePack.example
+import SourcePack.example.App;  
 
-public class TryPackage {
-    public static void main(String[] args) {
-        App ob = new App();
-    }
+// Defining a public class named TryPackage
+public class TryPackage {  
+    public static void main(String[] args) {  
+        
+        // Creating an object of the App class
+        App ob = new App();  
+        
+        // The object 'ob' can now be used to call methods from the App class (if defined)
+    }  
 }
