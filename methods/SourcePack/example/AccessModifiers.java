d2367420b356407e7b34670a4b910fde06205ff2
Chandan Saha <chandansaha1945@gmail.com>
2025-06-18 14:10:34 +0530

    update in Accessmodifier

--- methods/SourcePack/example/AccessModifiers.java
+++ methods/SourcePack/example/AccessModifiers.java
@@ -1,27 +1,38 @@
-package SourcePack.example;
-class Modifiers {
-    public String str = "This is the public string";
+// Defining a package named SourcePack.example
+package SourcePack.example;  
 
-    public void PrintFromClass() {
-        System.out.println("With in the Class in Public Access");
-    }
-}
+// A class named Modifiers with a public variable and a public method
+class Modifiers {  
+    // Public string variable, accessible from any class
+    public String str = "This is the public string";  
 
-public class AccessModifiers {
-    public static void main(String[] args) {
-        Modifiers ob = new Modifiers();
-        ob.PrintFromClass();
-        System.out.println(ob.str);
-        
-        outsider ob1 = new outsider();
-        ob1.PrintFromOutsideClass();
+    // Public method, accessible from anywhere within the package and outside
+    public void PrintFromClass() {  
+        System.out.println("Within the Class in Public Access");  
+    }  
+}  
 
-    }
-}
+// A public class named AccessModifiers with the main method
+public class AccessModifiers {  
+    public static void main(String[] args) {  
+        // Creating an object of Modifiers class and accessing its members
+        Modifiers ob = new Modifiers();  
+        ob.PrintFromClass();  // Calling the public method  
+        System.out.println(ob.str);  // Accessing the public variable  
 
-class outsider {
-    void PrintFromOutsideClass() {
-        Modifiers ob = new Modifiers();
-        System.out.println("With in Same code but from a outside class " + ob.str);
-    }
+        // Creating an object of outsider class and calling its method
+        outsider ob1 = new outsider();  
+        ob1.PrintFromOutsideClass();  
+    }  
+}  
+
+// A class named outsider (note: lowercase class name is unconventional, usually class names start with uppercase)
+class outsider {  
+    // Default (package-private) access method
+    void PrintFromOutsideClass() {  
+        // Creating an object of Modifiers class
+        Modifiers ob = new Modifiers();  
+        // Accessing the public variable from another class in the same package
+        System.out.println("Within Same code but from an outside class " + ob.str);  
+    }  
 }
