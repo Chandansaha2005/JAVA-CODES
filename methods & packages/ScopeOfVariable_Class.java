be1adf9b29448cf41556f56d6a0c2d24dea7b326
Chandan Saha <chandansaha1945@gmail.com>
2025-06-19 16:17:41 +0530

    class level scope

--- methods & packages/ScopeOfVariable_Class.java
+++ methods & packages/ScopeOfVariable_Class.java
@@ -1,17 +1,26 @@
 public class ScopeOfVariable_Class {
+    // Class-level variables (instance variables) accessible throughout the class
     int a = 10;
-    // int a==11;//In a same scope we can't have same named variable
+    // int a==11; // Invalid: Cannot declare another variable with the same name 'a' in the same scope
     int b = 5;
 
+    // Method to perform addition using class-level variables
     int add() {
-        return a + b;//we can use the variables anywhere in the class , this is class level scope
+        return a + b; // Class-level variables 'a' and 'b' used here
     }
 
+    // Method to perform subtraction using class-level variables
     int sub() {
-        return a - b;
+        return a - b; // Again, accessing the same variables within a different method
     }
 
+    // Main method - Entry point of the program
     public static void main(String[] args) {
+        // Creating an object to access non-static methods
+        ScopeOfVariable_Class obj = new ScopeOfVariable_Class();
         
+        // Calling methods and printing results
+        System.out.println("Addition: " + obj.add());
+        System.out.println("Subtraction: " + obj.sub());
     }
 }
