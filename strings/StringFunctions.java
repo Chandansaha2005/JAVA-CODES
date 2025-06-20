b502393ecfe4b4586ae377311e17a0949d872d39
Chandan Saha <chandansaha1945@gmail.com>
2025-06-20 23:34:12 +0530

    compareTo function

--- strings/StringFunctions.java
+++ strings/StringFunctions.java
@@ -4,5 +4,14 @@ public class StringFunctions {
         System.out.println(str.length());// 5
         System.out.println(str.charAt(4));// a
         System.out.println(str.indexOf('i'));// 0(AS it gives the 1st index whare it found )
+        String str1 = "abc";
+        String str2 = "abc";
+        System.out.println(str1.compareTo(str2)); // 0
+        str1 = "cbc";
+        str2 = "abc";
+        System.out.println(str1.compareTo(str2)); // 1
+        str1 = "abc";
+        str2 = "abz";
+        System.out.println(str1.compareTo(str2)); // -23
     }
 }
