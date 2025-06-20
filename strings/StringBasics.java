d1dc7ef1012f2249db0cdde456d3aee88aac2857
Chandan Saha <chandansaha1945@gmail.com>
2025-06-20 13:10:11 +0530

    update in string

--- strings/StringBasics.java
+++ strings/StringBasics.java
@@ -1,6 +1,13 @@
+// Define a public class named StringBasics
 public class StringBasics {
+
+    // Main method: the entry point of the program
     public static void main(String[] args) {
+        
+        // Declare and initialize a String variable with the value "Hello World"
         String str = "Hello World";
+        
+        // Print the string to the console
         System.out.println(str);
     }
 }
--- strings/StringInput.java
+++ strings/StringInput.java
@@ -1,15 +1,22 @@
+// Import the Scanner class from the java.util package for user input
 import java.util.*;
 
 public class StringInput {
     public static void main(String[] args) {
+        
+        // Create a Scanner object to read input from the console
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter a word = ");
-        // String str = sc.next(); // this will take only a word or character till spece
-        // System.out.println(str);
+        
+        // Prompt the user to enter a line of text
         System.out.print("Enter a Line = ");
-        String str1 = sc.nextLine(); // this will take only a whole line or character till Enter
+        
+        // Read the entire line of input until the user hits Enter
+        String str1 = sc.nextLine();
+        
+        // Display the entered line
         System.out.println(str1);
+        
+        // Close the Scanner to free system resources
         sc.close();
     }
-
 }
