81e279eaad853c9970d855396688541461914ac6
Chandan Saha <chandansaha1945@gmail.com>
2025-06-20 23:06:35 +0530

    charAt funtion

--- strings/StringFunctions.java
+++ strings/StringFunctions.java
@@ -1,6 +1,7 @@
 public class StringFunctions {
     public static void main(String[] args) {
         String str = "India";
-        System.out.println(str.length());//5
+        System.out.println(str.length());// 5
+        System.out.println(str.charAt(4));// a
     }
 }
