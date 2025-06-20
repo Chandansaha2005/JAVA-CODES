72a6a82a3301f80b718cc28d8e04b46c92c0b7ab
Chandan Saha <chandansaha1945@gmail.com>
2025-06-20 23:18:27 +0530

    indexOf funtion

--- strings/StringFunctions.java
+++ strings/StringFunctions.java
@@ -1,7 +1,8 @@
 public class StringFunctions {
     public static void main(String[] args) {
-        String str = "India";
+        String str = "india";
         System.out.println(str.length());// 5
         System.out.println(str.charAt(4));// a
+        System.out.println(str.indexOf('i'));// 0(AS it gives the 1st index whare it found )
     }
 }
