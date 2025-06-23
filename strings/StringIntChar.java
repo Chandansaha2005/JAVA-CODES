7c16ca5decc0badb4dc976067fc0a5b9c994df20
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 23:52:54 +0530

    string concat

--- strings/StringIntChar.java
+++ strings/StringIntChar.java
@@ -1,8 +1,16 @@
 public class StringIntChar {
     public static void main(String[] args) {
         String str = "chandan";
-        System.out.println(str + 1);
-        System.out.println(str + 'S');
-        System.out.println(str + " Saha");
+
+        // Concatenates the string with an integer. The integer 1 is converted to a
+        // string and appended.
+        System.out.println(str + 1); // Output: chandan1
+
+        // Concatenates the string with a character. 'S' is promoted to a string and
+        // appended.
+        System.out.println(str + 'S'); // Output: chandanS
+
+        // Concatenates the string with another string literal.
+        System.out.println(str + " Saha"); // Output: chandan Saha
     }
 }
