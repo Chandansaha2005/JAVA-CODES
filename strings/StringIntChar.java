0a78e3d3e2dee4c7c3f31e63fa654a4dc4fd0fa9
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 22:59:55 +0530

    String assign with char int string

---
+++ strings/StringIntChar.java
@@ -0,0 +1,8 @@
+public class StringIntChar {
+    public static void main(String[] args) {
+        String str = "chandan";
+        System.out.println(str + 1);
+        System.out.println(str + 'S');
+        System.out.println(str + " Saha");
+    }
+}
