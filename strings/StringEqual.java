0e82d149418deeebe28da9e3ddacf28390aa9ec4
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 23:38:41 +0530

    string equal

---
+++ strings/StringEqual.java
@@ -0,0 +1,9 @@
+public class StringEqual {
+    public static void main(String[] args) {
+        String s1 = "India";
+        String s2 = "India";
+        String s3 = new String("India");
+        System.out.println(s1 == s2);// true
+        System.out.println(s1 == s3);// false
+    }
+}
