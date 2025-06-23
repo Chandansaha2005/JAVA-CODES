a25c51050585ca2c8ebd2a906fb65aafd0a6a1c4
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 23:50:28 +0530

    Interning done

--- strings/StringInterningNew.java
+++ strings/StringInterningNew.java
@@ -1,8 +1,7 @@
 public class StringInterningNew {
     public static void main(String[] args) {
-        String str = "Hello";
-        String gtr = "Hello";
-        String str2 = new String("Hello");
-        
+        String str = "Hello"; // "Hello" goes into the String pool
+        String gtr = "Hello"; // Reuses the same pooled reference as str
+        String str2 = new String("Hello"); // Creates a new String object in the heap
     }
 }
