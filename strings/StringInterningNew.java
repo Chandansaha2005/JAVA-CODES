1661ba7de1a98041130e5f99a96972face198e17
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 23:17:03 +0530

    Interning and new in string

---
+++ strings/StringInterningNew.java
@@ -0,0 +1,8 @@
+public class StringInterningNew {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String gtr = "Hello";
+        String str2 = new String("Hello");
+        
+    }
+}
