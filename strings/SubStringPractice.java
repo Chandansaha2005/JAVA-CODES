906aad914a0ad532a4b46c6434ca45ecbab386d9
Chandan Saha <chandansaha1945@gmail.com>
2025-06-23 22:12:42 +0530

    substring practice

--- strings/SubStringPractice.java
+++ strings/SubStringPractice.java
@@ -1,3 +1,10 @@
 public class SubStringPractice {
-    
+    public static void main(String[] args) {
+        String str = "abcd";
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length()+1; j++) {
+                System.out.print(str.substring(i,j)+" ");
+            }
+        }
+    }
 }
