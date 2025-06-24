12ad8acfb278c248844e42cd1fb37023ff031795
Chandan Saha <chandansaha1945@gmail.com>
2025-06-25 01:42:04 +0530

    string Builder practice 2 done

--- strings/StringBuilderPractice2.java
+++ strings/StringBuilderPractice2.java
@@ -3,13 +3,12 @@ public class StringBuilderPractice2 {
         StringBuilder str = new StringBuilder("abc");
         int c = 0;
         for (int i = 0; i < str.length(); i++) {
-            for (int j = i; j < str.length() + 1; j++) {
-                System.out.print(str.subSequence(i, j) + " ");
-                int l = 0, k = str.subSequence(i, j).length();
-                boolean flag = false;
+            for (int j = i + 1; j <= str.length(); j++) {
+                int l = 0, k = str.subSequence(i, j).length() - 1;
+                boolean flag = true;
                 while (l < k) {
-                    if (str.subSequence(i, j).charAt(l) == str.subSequence(i, j).charAt(k)) {
-                        flag = true;
+                    if (str.subSequence(i, j).charAt(l) != str.subSequence(i, j).charAt(k)) {
+                        flag = false;
                     }
                     l++;
                     k--;
@@ -19,6 +18,6 @@ public class StringBuilderPractice2 {
                 }
             }
         }
-        System.out.println(c);
+        System.out.println("\nTotal palindromic substrings: " + c);
     }
 }
