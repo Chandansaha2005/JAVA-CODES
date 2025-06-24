19d83886b6b0a36527d36c12839b17c5c01937ee
Chandan Saha <chandansaha1945@gmail.com>
2025-06-25 01:31:21 +0530

    string practice

--- strings/StringPractice2.java
+++ strings/StringPractice2.java
@@ -1,18 +1,27 @@
 public class StringPractice2 {
+    public static boolean IsPalindrome(String s) {
+        int l = 0, k = s.length() - 1;
+        while (l < k) {
+            if (s.charAt(l) != s.charAt(k)) {
+                return false;
+            }
+            l++;
+            k--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String str = "abc";
-        String rtr ;
         int c = 0;
         for (int i = 0; i < str.length(); i++) {
             for (int j = i; j < str.length() + 1; j++) {
-                System.out.print(str.substring(i, j) + " ");
-                rtr=str.substring(i, j);
-                if (rtr.equals(rtr.)) {
-                    
-                } else {
-                    
+                if (IsPalindrome(str.substring(i, j))==true) {
+                    System.out.print(str.substring(i, j) + " ");
+                    c++;
                 }
             }
         }
+        System.out.println(c);
     }
 }
