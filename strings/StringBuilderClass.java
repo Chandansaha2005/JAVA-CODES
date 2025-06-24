dd9c3cf9daf145d6b0984e2001723fe6934569d3
Chandan Saha <chandansaha1945@gmail.com>
2025-06-24 22:59:10 +0530

    string builder setcharat

--- strings/StringBuilderClass.java
+++ strings/StringBuilderClass.java
@@ -1,7 +1,10 @@
 public class StringBuilderClass {
     public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("Hello world");
-        // str = str +"world";// this will not work here
-        str.append("World");
+        StringBuilder str = new StringBuilder("Hello");
+        // str = str +"World";// this will not work here
+        str.append(" World");
+        // str.charAt(0)='M';// this didn't work in string
+        str.setCharAt(0, 'M');
+        System.out.println(str);
     }
 }
