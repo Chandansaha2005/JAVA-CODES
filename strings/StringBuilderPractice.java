ebd631ac18389cff4bf7f733d529e9607591e386
Chandan Saha <chandansaha1945@gmail.com>
2025-06-24 23:55:24 +0530

    string builder practice 1 done

--- strings/StringBuilderPractice.java
+++ strings/StringBuilderPractice.java
@@ -1,18 +1,15 @@
 public class StringBuilderPractice {
     public static void main(String[] args) {
-        int assci = 0,;
         StringBuilder str = new StringBuilder("ChanDAN");
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
-                assci = ((int) (str.charAt(i)))-32;
-                //System.out.print(assci + " ");
-                System.out.print((char) assci + " ");
+                int assci = ((int) str.charAt(i)) - 32;
                 str.setCharAt(i, (char) assci);
             } else {
-                assci = ((int) str.charAt(i))+32 ;
-                System.out.print((char) assci + " ");
+                int assci = ((int) str.charAt(i)) + 32;
                 str.setCharAt(i, (char) assci);
             }
         }
+        System.out.println(str);
     }
 }
