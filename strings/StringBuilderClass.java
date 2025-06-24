95280a4a970c35d75d6064ef4e9fe485b7c64581
Chandan Saha <chandansaha1945@gmail.com>
2025-06-24 22:52:48 +0530

    String bulilder append

--- strings/StringBuilderClass.java
+++ strings/StringBuilderClass.java
@@ -1,6 +1,7 @@
-import java.util.*;
 public class StringBuilderClass {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Hello world");
+        // str = str +"world";// this will not work here
+        str.append("World");
     }
 }
