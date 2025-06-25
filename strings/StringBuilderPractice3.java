cd112a4b68e4d48dcf6a31a9d44e96057fa269c9
Chandan Saha <chandansaha1945@gmail.com>
2025-06-25 14:47:25 +0530

    string builder practice 3

---
+++ strings/StringBuilderPractice3.java
@@ -0,0 +1,12 @@
+public class StringBuilderPractice3 {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("I am Chandan Saha");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+            String temp = str.charAt(i);
+            if () {
+
+            }
+        }
+    }
+}
---
+++ strings/StringPractice3.java
@@ -0,0 +1,13 @@
+public class StringPractice3 {
+    public static void main(String[] args) {
+        String str = "I am Chandan Saha";
+        char temp;
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+            temp= (char)str.charAt(i);
+            if (temp ==) {
+
+            }
+        }
+    }
+}
