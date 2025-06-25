e1908d0be9e48a4597feb4d0e6a07b91d380405a
Chandan Saha <chandansaha1945@gmail.com>
2025-06-25 15:16:59 +0530

    StringBuilder practice 3 done

--- strings/StringBuilderPractice3.java
+++ strings/StringBuilderPractice3.java
@@ -1,12 +1,16 @@
 public class StringBuilderPractice3 {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("I am Chandan Saha");
-        for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.charAt(i));
-            String temp = str.charAt(i);
-            if () {
-
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0, j = 0; i < str.length(); i++) {
+            char temp = (char) str.charAt(i);
+            if (temp == ' ' || i == str.length() - 1) {
+                StringBuilder rtr = new StringBuilder();
+                rtr.append(str.subSequence(j, i + 1));
+                ans.append(rtr.reverse());
+                j = i;
             }
         }
+        System.out.println(ans);
     }
 }
--- strings/StringPractice3.java
+++ strings/StringPractice3.java
@@ -1,11 +1,10 @@
 public class StringPractice3 {
     public static void main(String[] args) {
         String str = "I am Chandan Saha";
-        char temp;
+        String temp;
         for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.charAt(i));
-            temp= (char)str.charAt(i);
-            if (temp ==) {
+            
+            if (str.charAt(i).equals(" ")) {
 
             }
         }
