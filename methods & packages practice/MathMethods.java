542a286c9be650e3b3a747f2bcafa80883f157a2
Chandan Saha <chandansaha1945@gmail.com>
2025-06-26 11:46:44 +0530

    importent math mathods

--- methods & packages practice/MathMethods.java
+++ methods & packages practice/MathMethods.java
@@ -1,5 +1,11 @@
 public class MathMethods {
     public static void main(String[] args) {
-        System.out.println(Math.min(5, 6));
+        System.out.println(Math.min(5, 6));// 5
+        System.out.println(Math.max(4, 8));// 8
+        System.out.println(Math.abs(-6));// 6
+        System.out.println(Math.sqrt(8));// 2.8284271247461903
+        System.out.println(Math.pow(2, 5));// 32.0
+        System.out.println(Math.ceil(4.5));// 5.0
+        System.out.println(Math.floor(4.5));// 4.0
     }
 }
