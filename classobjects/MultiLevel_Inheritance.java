78cb049553e3942732bd422cd503bd15c338ed7f
Chandan Saha <chandansaha1945@gmail.com>
2025-06-27 22:58:34 +0530

    Multi level inheritance done

--- classobjects/MultiLevel_Inheritance.java
+++ classobjects/MultiLevel_Inheritance.java
@@ -9,9 +9,53 @@ public class MultiLevel_Inheritance {
         a = sc.nextInt();
         System.out.println("Enter the 2nd number = ");
         b = sc.nextInt();
+        sc.close();
     }
 
     public void add() {
         sum = a + b;
     }
+}
+
+class Child1 extends MultiLevel_Inheritance {
+    int sub;
+
+    public void subtract() {
+        sub = a - b;
+    }
+}
+
+class Child2 extends Child1 {
+    int mul;
+
+    public void multi() {
+        mul = a * b;
+    }
+}
+
+class Child3 extends Child2 {
+    int div;
+
+    public void divide() {
+        div = a / b;
+    }
+
+    public void show() {
+        System.out.println("Addition = " + sum);
+        System.out.println("Subtraction = " + sub);
+        System.out.println("Multiplecation = " + mul);
+        System.out.println("Division = " + div);
+    }
+}
+
+class Child4 extends Child3 {
+    public static void main(String[] args) {
+        Child4 ob = new Child4();
+        ob.input();
+        ob.add();
+        ob.subtract();
+        ob.multi();
+        ob.show();
+    }
+
 }
