1100d01e55813c597c05d864668a3f73129bc524
Chandan Saha <chandansaha1945@gmail.com>
2025-06-27 22:45:06 +0530

    calculator as an example of multilevel inheritance

--- classobjects/MultiLevel_Inheritance.java
+++ classobjects/MultiLevel_Inheritance.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class MultiLevel_Inheritance {
-    
+    int a, b, sum;
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st number = ");
+        a = sc.nextInt();
+        System.out.println("Enter the 2nd number = ");
+        b = sc.nextInt();
+    }
+
+    public void add() {
+        sum = a + b;
+    }
 }
