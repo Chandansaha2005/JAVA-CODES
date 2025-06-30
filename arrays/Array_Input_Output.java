776381195e6b64606630d329b2bf5ab9c9729287
Chandan Saha <chandansaha1945@gmail.com>
2025-06-30 22:05:43 +0530

    array input output

---
+++ arrays/Array_Input_Output.java
@@ -0,0 +1,15 @@
+public class Array_Input_Output {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 9;
+        arr[1] = 27;
+        arr[2] = 35;
+        arr[3] = 4;
+        arr[4] = 15;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
