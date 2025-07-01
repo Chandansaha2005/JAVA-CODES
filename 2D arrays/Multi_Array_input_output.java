36a66046c99ada882a5149360bae4424ec521f48
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 13:52:46 +0530

    2d array input output

---
+++ 2D arrays/Multi_Array_input_output.java
@@ -0,0 +1,22 @@
+public class Multi_Array_input_output {
+    public static void main(String[] args) {
+        int arr[][] = new int[2][4];
+        arr[0][0] = 2;
+        arr[0][1] = 5;
+        arr[0][2] = 6;
+        arr[0][3] = 7;
+        arr[1][0] = 4;
+        arr[1][1] = 3;
+        arr[1][2] = 5;
+        arr[1][3] = 9;
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[0][2]);
+        System.out.println(arr[0][3]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
+        System.out.println(arr[1][2]);
+        System.out.println(arr[1][3]);
+
+    }
+}
