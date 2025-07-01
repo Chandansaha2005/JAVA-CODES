7ec5c1d68d52ddef8d69034a037aacce32a3abba
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 21:21:55 +0530

    find an element in an array

--- 1D arrays/Array_FindAnElement.java
+++ 1D arrays/Array_FindAnElement.java
@@ -1,5 +1,19 @@
 public class Array_FindAnElement {
+    static int Find(int a[], int f) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == f) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        
+        int arr[] = { 1, 5, 3 };
+        int x = 5;
+        if (Find(arr, x) > -1) {
+            System.out.print(x + " Found At = index " + Find(arr, x));
+        } else
+            System.out.print("Not Found");
     }
 }
