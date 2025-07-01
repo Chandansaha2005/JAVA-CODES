b34f77774b11fd49e2fc7a339a024d4f17e4e4f0
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 18:27:52 +0530

    array printing with loop

---
+++ 1D arrays/Array_Traversing_Printing.java
@@ -0,0 +1,13 @@
+public class Array_Traversing_Printing {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 14;
+        arr[1] = 8;
+        arr[2] = 15;
+        arr[3] = 31;
+        arr[4] = 24;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
