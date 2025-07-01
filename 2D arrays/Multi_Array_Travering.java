57cef56717388f98e9db520bcff80dea0ba2a6e4
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 20:11:46 +0530

    array printing using loops done

--- 2D arrays/Multi_Array_Travering.java
+++ 2D arrays/Multi_Array_Travering.java
@@ -1,6 +1,7 @@
 public class Multi_Array_Travering {
     public static void main(String[] args) {
         int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+        // With for loop
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
@@ -8,6 +9,7 @@ public class Multi_Array_Travering {
             System.out.println();
         }
         System.out.println();
+        // With for each loop
         for (int a[] : arr) {
             for (int b : a) {
                 System.out.print(b + " ");
@@ -15,11 +17,12 @@ public class Multi_Array_Travering {
             System.out.println();
         }
         System.out.println();
-        int c = 0, d;
+        // With while loop
+        int c = 0, d = 0;
         while (c < arr.length) {
             d = 0;
             while (d < arr[c].length) {
-                System.out.print(arr[d]);
+                System.out.print(arr[c][d] + " ");
                 d++;
             }
             c++;
