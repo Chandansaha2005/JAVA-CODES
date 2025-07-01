775c5e9100d77d69f1e79050047cbc101e2b0a82
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 13:58:32 +0530

    2d array literal

---
+++ 2D arrays/Multi_Array_Literal.java
@@ -0,0 +1,9 @@
+public class Multi_Array_Literal {
+    public static void main(String[] args) {
+        String[][] arr = { { "Chandan", "Satyam" }, { "Puskar", "Mora" } };
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
+    }
+}
