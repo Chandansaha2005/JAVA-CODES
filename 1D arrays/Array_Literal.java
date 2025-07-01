7225453f17f018a602131d14f1af782c8da2c409
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 14:00:47 +0530

    array literal

---
+++ 1D arrays/Array_Literal.java
@@ -0,0 +1,13 @@
+public class Array_Literal {
+    public static void main(String[] args) {
+        int arr[] = { 2, 5, 3, 6, 4, 8, 9, 7 };
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+        System.out.println(arr[7]);
+    }
+}
