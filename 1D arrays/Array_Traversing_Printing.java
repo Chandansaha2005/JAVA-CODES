ed6ab8ce5efc7318e278fad1a23038c7c8262b03
Chandan Saha <chandansaha1945@gmail.com>
2025-07-01 18:33:55 +0530

    array printing with With for each loop

--- 1D arrays/Array_Traversing_Printing.java
+++ 1D arrays/Array_Traversing_Printing.java
@@ -6,8 +6,12 @@ public class Array_Traversing_Printing {
         arr[2] = 15;
         arr[3] = 31;
         arr[4] = 24;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {// With for loop
             System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int a : arr) {// With for each loop
+            System.out.print(a + " ");
         }
     }
 }
