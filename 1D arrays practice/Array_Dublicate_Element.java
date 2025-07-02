093daf63f59a69da6249f52286a1c5c4f6c28d1c
Chandan Saha <chandansaha1945@gmail.com>
2025-07-02 14:41:34 +0530

    find the last occurence of dublicate element

--- 1D arrays practice/Array_Dublicate_Element.java
+++ 1D arrays practice/Array_Dublicate_Element.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class Array_Dublicate_Element {
     static void checkDublicate(int a[], int x) {
-        int c = 0;
+        int c = 0, l = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] == x) {
                 c++;
                 System.out.println(x + " Present At Index " + i);
+                l = i;
             }
         }
-        System.out.print(x + " Present " + c + " Times");
+        if (c != 0) {
+            System.out.println(x + " Last Occurence found At Index " + l);
+            System.out.print(x + " Present " + c + " Times");
+        } else
+            System.out.print(x + " is Not Present in This Array");
     }
 
     public static void main(String[] args) {
