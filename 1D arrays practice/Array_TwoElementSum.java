f01c5b6b98aaa48fbaf06241432426a832184374
Chandan Saha <chandansaha1945@gmail.com>
2025-07-03 22:37:49 +0530

    find the pair of elements in an array , which sum will be equals to "target

---
+++ 1D arrays practice/Array_TwoElementSum.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Array_TwoElementSum {
+
+    static void targetSum(int a[], int t) {
+        int f = 0;
+        System.out.println("Pairs Which Sum = " + t + " :-");
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] + a[j] == t) {
+                    System.out.println("(" + a[i] + "," + a[j] + ")");
+                    f++;
+                }
+            }
+        }
+        if (f == 0) {
+            System.out.print("No pair found");
+        }
+    }
+
+    static void printArray(int a[]) {
+        System.out.print("\nArray = ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number Of Element = ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the Element(s)= ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        printArray(arr);
+        System.out.print("Enter the Target = ");
+        int t = sc.nextInt();
+        targetSum(arr, t);
+        sc.close();
+    }
+}
