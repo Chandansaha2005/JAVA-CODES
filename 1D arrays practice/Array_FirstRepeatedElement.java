ad06ecafd4bedbe25fd33eceac4dba921fa68bb3
Chandan Saha <chandansaha1945@gmail.com>
2025-07-04 23:21:57 +0530

    First element that repeating in array

--- 1D arrays practice/Array_FirstRepeatedElement.java
+++ 1D arrays practice/Array_FirstRepeatedElement.java
@@ -6,11 +6,11 @@ public class Array_FirstRepeatedElement {
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[j] == a[i]) {
-                    res = a[i];
+                    return a[i];
                 }
             }
         }
-        return res;
+        return -1;
     }
 
     static void printArray(int a[]) {
