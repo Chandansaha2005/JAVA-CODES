b89875e24e373f703d0e3c978cab4235d9827730
Chandan Saha <chandansaha1945@gmail.com>
2025-07-05 21:43:07 +0530

    rotation of any array properly done

--- 1D arrays practice/Array_Rotation.java
+++ 1D arrays practice/Array_Rotation.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
 public class Array_Rotation {
-    static int[] RotateArray(int a[]) {
-        int temp = a[a.length - 1];
+    static int[] RotateArray(int a[], int k) {
+        int l = a.length;
+        if (k > l + 1)
+            k = k % 2;
+        for (int j = 0; j < k; j++) {
+            int temp = a[l - 1];
 
-        for (int i = a.length - 2; i >= 0; i--) {
-            a[i + 1] = a[i];
+            for (int i = l - 2; i >= 0; i--) {
+                a[i + 1] = a[i];
+            }
+            a[0] = temp;
         }
-        a[0] = temp;
         return a;
     }
 
@@ -31,9 +36,10 @@ public class Array_Rotation {
         System.out.print("Enter The Number of Rotation you want = ");
         k = sc.nextInt();
         System.out.println("After " + k + " Rotation :-");
-        for (int i = 0; i < k; i++) {
-            printArray(RotateArray(arr));
-        }
+        // for (int i = 0; i < k; i++) {
+        // printArray(RotateArray(arr));
+        // }
+        printArray(RotateArray(arr, k));
         sc.close();
     }
 }
