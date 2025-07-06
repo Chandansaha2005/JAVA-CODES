86279663127510531b80f2f080d9a5ca3fc59fa7
Chandan Saha <chandansaha1945@gmail.com>
2025-07-06 11:41:41 +0530

    array sorting of non decreasing elements

--- 1D arrays practice/Array_NonDecreasingSquare.java
+++ 1D arrays practice/Array_NonDecreasingSquare.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
-public class Array_SortingEvenOdd {
-    static int[] sortEvenOdd(int a[]) {
+public class Array_NonDecreasingSquare {
+    static int[] squareArray(int a[]) {
         int i = 0, j = a.length - 1;
         while (i < j) {
-            if (a[i] % 2 == 1 && a[j] % 2 == 0) {
-                int temp = a[i];
-                a[i] = a[j];
+            if (Math.abs(a[i]) > Math.abs(a[j])) {
+                int temp = (int) Math.pow(a[i], 2);
+                a[i] = (int) Math.pow(a[j], 2);
                 a[j] = temp;
                 i++;
                 j--;
-            }
-            if (a[i] % 2 == 0) {
+            } else {
+                a[i] = (int) Math.pow(a[i], 2);
+                a[j] = (int) Math.pow(a[i], 2);
                 i++;
-            }
-            if (a[j] % 2 == 1) {
-                j--;
             }
 
         }
@@ -35,12 +33,12 @@ public class Array_SortingEvenOdd {
         System.out.print("Enter The Number Of Element = ");
         int n = sc.nextInt();
         int arr[] = new int[n];
-        System.out.print("Enter the Elements = ");
+        System.out.print("Enter the Elements in Sorted manner = ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         printArray(arr);
-        printArray(sortEvenOdd(arr));
+        printArray(squareArray(arr));
         sc.close();
     }
 }
