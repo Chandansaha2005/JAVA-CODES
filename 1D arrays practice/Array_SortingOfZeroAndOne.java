6ba303043b9f8bff91feaf357c486df7674f59bb
Chandan Saha <chandansaha1945@gmail.com>
2025-07-06 10:43:03 +0530

    sorting of an array which contains 0s and 1s with less time

--- 1D arrays practice/Array_SortingOfZeroAndOne.java
+++ 1D arrays practice/Array_SortingOfZeroAndOne.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
 
 public class Array_SortingOfZeroAndOne {
+    // static int[] sortZeroOne(int a[]) {
+    // int zero = 0, one = 0;
+    // for (int i = 0; i < a.length; i++) {
+    // if (a[i] == 0)
+    // zero++;
+    // else
+    // one++;
+    // }
+    // for (int i = 0; i < a.length; i++) {
+    // if (i < zero) {
+    // a[i] = 0;
+    // } else
+    // a[i] = 1;
+    // }
+    // return a;
+    // }
     static int[] sortZeroOne(int a[]) {
-        int zero = 0, one = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == 0)
-                zero++;
-            else
-                one++;
-        }
-        for (int i = 0; i < a.length; i++) {
-            if (i < zero) {
-                a[i] = 0;
-            } else
-                a[i] = 1;
+        int i = 0, j = a.length - 1;
+        while (i < j) {
+            if (a[i] == 1 && a[j] == 0) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+            if (a[i] == 0) {
+                i++;
+            }
+            if (a[j] == 1) {
+                j--;
+            }
+
         }
         return a;
     }
