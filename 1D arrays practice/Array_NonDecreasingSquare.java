321f5619a9678c790ea86fd0073f72bc9d062217
Chandan Saha <chandansaha1945@gmail.com>
2025-07-07 16:11:02 +0530

    array sorting of non decreasing elements

--- 1D arrays practice/Array_NonDecreasingSquare.java
+++ 1D arrays practice/Array_NonDecreasingSquare.java
@@ -4,18 +4,17 @@ public class Array_NonDecreasingSquare {
     static int[] squareArray(int a[]) {
         int i = 0, j = a.length - 1;
         while (i < j) {
-            if (Math.abs(a[i]) > Math.abs(a[j])) {
-                int temp = (int) Math.pow(a[i], 2);
-                a[i] = (int) Math.pow(a[j], 2);
+            if (a[i] * a[i] > a[j] * a[j]) {
+                int temp = a[i] * a[i];
+                a[i] = a[j] * a[j];
                 a[j] = temp;
-                i++;
                 j--;
+                i++;
             } else {
-                a[i] = (int) Math.pow(a[i], 2);
-                a[j] = (int) Math.pow(a[i], 2);
+                a[i] = a[i] * a[i];
+                a[j] = a[j] * a[j];
                 i++;
             }
-
         }
         return a;
     }
