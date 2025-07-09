2dce4bdf31666fce148b1e69120e9485da6cb1da
Chandan Saha <chandansaha1945@gmail.com>
2025-07-09 23:35:33 +0530

    suufix sum of array done

--- 1D arrays practice/Array_Suffix_Sum.java
+++ 1D arrays practice/Array_Suffix_Sum.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
 
 public class Array_Suffix_Sum {
+    static void suffixSum(int arr[]) {
+        for (int i = arr.length - 2; i >= 0; i--) {
+            arr[i] = arr[i] + arr[i + 1];
+        }
+        printArray(arr);
+    }
+
+    static void printArray(int arr[]) {
+        System.out.print("Array = ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements = ");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         int arr[] = new int[n];
+        System.out.print("Enter the elements = ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        printArray(arr);
+        suffixSum(arr);
+        sc.close();
     }
 }
