8dd54dc0ceaf3a5384186a1ddb4b8db910256457
Chandan Saha <chandansaha1945@gmail.com>
2025-07-09 23:18:48 +0530

    array  suffix sum

---
+++ 1D arrays practice/Array_Suffix_Sum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Array_Suffix_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements = ");
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+    }
+}
