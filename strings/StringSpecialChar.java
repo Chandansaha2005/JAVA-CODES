5425c59e06b5f2231cb1d05cc1a6e00c08fc91e1
Chandan Saha <chandansaha1945@gmail.com>
2025-07-10 22:16:53 +0530

    practice

--- strings/StringSpecialChar.java
+++ strings/StringSpecialChar.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class StringSpecialChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        StringBuilder ntr = new StringBuilder();
+        System.out.println((int) str.charAt(0));
+        for (int i = 0, j = 0; i < str.length() && j < str.length(); i++) {
+            if ((int) str.charAt(i) >= 97 && (int) str.charAt(i) <= 122) {
+                ntr.setCharAt(j++, str.charAt(i));
+            }
+        }
+        System.out.println(ntr);
+        sc.close();
     }
 }
