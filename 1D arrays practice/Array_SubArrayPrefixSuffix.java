131e38e643d2bf19920dc8c9789ac0a11ce1de4b
Chandan Saha <chandansaha1945@gmail.com>
2025-07-10 23:14:01 +0530

    suffix and prefix sum practice

---
+++ 1D arrays practice/Array_SubArrayPrefixSuffix.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Array_SubArrayPrefixSuffix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System)
+        int arr[]=new int[]
+    }
+}
--- strings/StringSpecialChar.java
+++ strings/StringSpecialChar.java
@@ -5,10 +5,10 @@ public class StringSpecialChar {
         Scanner sc = new Scanner(System.in);
         StringBuilder str = new StringBuilder(sc.nextLine());
         StringBuilder ntr = new StringBuilder();
-        System.out.println((int) str.charAt(0));
-        for (int i = 0, j = 0; i < str.length() && j < str.length(); i++) {
-            if ((int) str.charAt(i) >= 97 && (int) str.charAt(i) <= 122) {
-                ntr.setCharAt(j++, str.charAt(i));
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
+                ntr.append(ch);
             }
         }
         System.out.println(ntr);
