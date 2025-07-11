d155d397c08e5f54d0aa32db354b3c4797c7f9c0
Chandan Saha <chandansaha1945@gmail.com>
2025-07-11 22:56:16 +0530

    array practice

--- 1D arrays practice/Array_SubArrayPrefixSuffix.java
+++ 1D arrays practice/Array_SubArrayPrefixSuffix.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Array_SubArrayPrefixSuffix {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements  =");
         int arr[]=new int[]
     }
 }
