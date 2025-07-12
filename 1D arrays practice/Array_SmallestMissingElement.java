0cfca85619df85a0ee3b919380a3eb9b574a8d77
Chandan Saha <chandansaha1945@gmail.com>
2025-07-12 20:58:57 +0530

    array find the smallest positive number in a sorted array

---
+++ 1D arrays practice/Array_SmallestMissingElement.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_SmallestMissingElement {
+    static Scanner sc = new Scanner(System.in);
+
+    static int smallestMissing(int arr[]) {
+        int num = 0;
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] - 1 != arr[i - 1]) {
+                num = arr[i] - 1;
+            }
+        }
+        return num;
+    }
+
+    static void printArray(int arr[]) {
+        System.out.print("Array = ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void input(int arr[]) {
+        System.out.print("Enter the elements = ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number of elements = ");
+        int arr[] = new int[sc.nextInt()];
+        input(arr);
+        printArray(arr);
+        Arrays.sort(arr);
+        System.out.print("Product of all Element in The Array = " + smallestMissing(arr));
+    }
+}
