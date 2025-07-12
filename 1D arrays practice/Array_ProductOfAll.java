c50668407eedd0fa45a44ed87a7723dd2c90702c
Chandan Saha <chandansaha1945@gmail.com>
2025-07-12 20:12:39 +0530

    Arra product of all elements

--- 1D arrays practice/Array_ProductOfAll.java
+++ 1D arrays practice/Array_ProductOfAll.java
@@ -1,24 +1,7 @@
 import java.util.Scanner;
 
-public class Array_SubArrayPrefixSuffix {
+public class Array_ProductOfAll {
     static Scanner sc = new Scanner(System.in);
-
-    static int suffixSum(int arr[], int n) {
-        for (int i = n - 2; i >= 0; i--) {
-            arr[i] = arr[i] + arr[i + 1];
-        }
-        printArray(arr);
-        return arr[n];
-    }
-
-    static int prefix(int arr[], int n) {
-        int f = 0;
-        for (int i = 0; i < arr.length; i++) {
-            f += arr[i];
-            arr[i] = f;
-        }
-        return n;
-    }
 
     static void printArray(int arr[]) {
         System.out.print("Array = ");
@@ -35,12 +18,9 @@ public class Array_SubArrayPrefixSuffix {
     }
 
     public static void main(String[] args) {
-
         System.out.print("Enter the number of elements = ");
         int arr[] = new int[sc.nextInt()];
         input(arr);
         printArray(arr);
-        System.out.print(prefix(arr, arr.length));
-        System.out.print(suffixSum(arr, arr.length));
     }
 }
