849a6f07c354ee4e86e0d2b4b050b45a2d396432
Chandan Saha <chandansaha1945@gmail.com>
2025-07-13 23:09:45 +0530

    array factorial

---
+++ 1D arrays practice/Array_Factorial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Array_Factorial {
+    static Scanner sc = new Scanner(System.in);
+
+    static int multiply(int j, int arr[], int size) {
+        int rem = 0;
+        for (int i = 1, j = 0; i <= n; i = n / 10) {
+
+        }
+        return arr;
+    }
+
+    static void factorial(int n) {
+        int arr[] = new int[1000];
+        arr[0] = 1;
+        int size = 1;
+        for (int i = 2; i <= n; i++) {
+            size = multiply(i, arr, size);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number = ");
+        printArray(input(arr, factorial(sc.nextInt())));
+    }
+}
