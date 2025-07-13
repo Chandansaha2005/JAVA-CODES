e5d32f9c517894a5070eaf93d70817750d491ac7
Chandan Saha <chandansaha1945@gmail.com>
2025-07-13 16:49:32 +0530

    largest three done

--- 1D arrays practice/Array_LargestThreeElement.java
+++ 1D arrays practice/Array_LargestThreeElement.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 public class Array_LargestThreeElement {
     static Scanner sc = new Scanner(System.in);
 
-    static int[] largestThree(int arr[]) {
-        int ans[] = { 0, 0, 0 };
-        for (int i = 1; i <= ans.length; i++) {
-            for (int j = 1; j < arr.length; j++) {
-                if (arr[j] < arr[j - 1] && arr[j - 1] != ans[i - 1]) {
-                    ans[i - 1] = arr[j - 1];
-                }
-            }
+    static void largestThree(int arr[]) {
+        if (arr.length < 3) {
+            printArray(arr);
+            return;
         }
-        return ans;
+        for (int i = arr.length - 1; i > arr.length - 4; i--) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     static void printArray(int arr[]) {
-        System.out.print("Array = ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
@@ -34,9 +31,8 @@ public class Array_LargestThreeElement {
         System.out.print("Enter the number of elements = ");
         int arr[] = new int[sc.nextInt()];
         input(arr);
-        printArray(arr);
         Arrays.sort(arr);
         System.out.print("Largest three Number in The Array = ");
-        printArray(largestThree(arr));
+        largestThree(arr);
     }
 }
