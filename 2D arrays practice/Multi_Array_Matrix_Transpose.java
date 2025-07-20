3e385ec3b63de7bc6f56c144f740d7dfc3d2c5ff
Chandan Saha <chandansaha1945@gmail.com>
2025-07-21 00:56:29 +0530

    transpose of a matrix done

--- 2D arrays practice/Multi_Array_Matrix_Transpose.java
+++ 2D arrays practice/Multi_Array_Matrix_Transpose.java
@@ -1,3 +1,50 @@
+import java.util.Scanner;
+
 public class Multi_Array_Matrix_Transpose {
-    
+    static Scanner sc = new Scanner(System.in);
+
+    static int[][] Transpose(int arr[][], int r, int c) {
+        int[][] trans = new int[c][r];
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < r; j++) {
+                trans[i][j] = arr[j][i];
+            }
+        }
+        return trans;
+    }
+
+    static void input2dArray(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.err.print("Enter the Element of " + (i + 1) + " no. row & " + (j + 1) + " no. Column = ");
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+    }
+
+    static void print2dArray(int arr[][]) {
+        System.out.println("Matrix:- ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter the no. of Rows of Matrix = ");
+        int r = sc.nextInt();
+        System.out.print("Enter the no. of Column of Matrix = ");
+        int c = sc.nextInt();
+        int arr[][] = new int[r][c];
+        System.out.println("Enter Elements:-");
+        input2dArray(arr);
+        print2dArray(arr);
+        System.out.println("Transpose Of This Matrix :-");
+        print2dArray(Transpose(arr, r, c));
+        sc.close();
+    }
 }
