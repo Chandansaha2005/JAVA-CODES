6cae03d7889bec1e32f6a62cb14277a01e6ced65
Chandan Saha <chandansaha1945@gmail.com>
2025-07-20 21:32:42 +0530

    2d array element

--- 2D arrays/Multi_Array_MatrixCreation.java
+++ 2D arrays/Multi_Array_MatrixCreation.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Multi_Array_MatrixCreation {
-    
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.err.print("Enter the Elements of " + (i + 1) + " no. Row = ");
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
