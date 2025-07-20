af0916064554aeee83ebbc489a8316153679d0b0
Chandan Saha <chandansaha1945@gmail.com>
2025-07-20 22:59:26 +0530

    Addition of two matrix

---
+++ 2D arrays/Multi_Array_MatrixAddition.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Multi_Array_MatrixAddition {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of Rows = ");
+        int r = sc.nextInt();
+        System.out.print("Enter the no. of Columns = ");
+        int c = sc.nextInt();
+        int arr[][] = new int[r][c];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.err.print("Enter the Element of " + (i + 1) + " no. row & " + (j + 1) + " no. Column = ");
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
