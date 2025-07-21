678e5595be388b4e7c02946b8024dffc41dd58bd
Chandan Saha <chandansaha1945@gmail.com>
2025-07-21 11:18:59 +0530

    gfg nation up qns

--- operators practice/Divisible_By_13.java
+++ operators practice/Divisible_By_13.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Divisible_By_13 {
+    static boolean Solution(String s) {
+        int n = Integer.parseInt(s);
+        return (n % 13 == 0);
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        System.out.println(Solution(sc.next()));
+        sc.close();
     }
 }
