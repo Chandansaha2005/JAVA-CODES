efaaf578d8e4a319196bd59409cb486c344ef4d9
Chandan Saha <chandansaha1945@gmail.com>
2025-07-21 08:44:21 +0530

    spiral matrix

--- 2D arrays practice/Multi_Array_SpiralMatrix.java
+++ 2D arrays practice/Multi_Array_SpiralMatrix.java
@@ -1,11 +1,39 @@
 import java.util.Scanner;
 
 public class Multi_Array_SpiralMatrix {
-    static int[][] spiralMatrix(int n){
-        int arr[][]=new int[n-1][n-1];
-        
+    static int[][] spiralMatrix(int r, int c) {
+        int arr[][] = new int[r][c];
+        // topRow -> lastColumn -> buttomRow -> firstColumn
+        // -> 2ndTopRow -> 2ndLastColumn -> 2ndButtomRow -> 2ndFirstColumn
+        // -> And so on.......
+        int topRow = 0, buttomRow = r - 1, firstColumn = 0, lastColumn = c - 1;
+        int total = 0;
+        System.out.println("Elements in Spiral Order :--");
+        while (total < r * c) {
+            // topRow -> firstColumn to lastColumn
+            for (int i = firstColumn; i <= lastColumn && total < r * c; i++) {
+                System.out.print(total++ + " ");
+            }
+            topRow++;
+            // lastColumn -> topRow to buttomRow
+            for (int i = topRow; i <= buttomRow && total < r * c; i++) {
+                System.out.print(total++ + " ");
+            }
+            lastColumn--;
+            // buttomRow -> lastColumn to firstColumn
+            for (int i = lastColumn; i >= firstColumn && total < r * c; i--) {
+                System.out.print(total++ + " ");
+            }
+            buttomRow--;
+            // firstColumn -> buttomRow to topRow
+            for (int i = buttomRow; i >= topRow && total < r * c; i--) {
+                System.out.print(total++ + " ");
+            }
+            firstColumn++;
+        }
         return arr;
     }
+
     static void print2dArray(int arr[][]) {
         System.out.println("Matrix:- ");
         for (int i = 0; i < arr.length; i++) {
@@ -16,12 +44,13 @@ public class Multi_Array_SpiralMatrix {
         }
     }
 
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. of Rows = ");
-        int n = sc.nextInt();
-        print2dArray(spiralMatrix(n));
+        int r = sc.nextInt();
+        System.out.print("Enter the no. of Columns = ");
+        int c = sc.nextInt();
+        print2dArray(spiralMatrix(r, c));
         sc.close();
     }
 }
