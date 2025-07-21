7d7dc82daa08671a46b7befae667cc66196fb06c
Chandan Saha <chandansaha1945@gmail.com>
2025-07-21 11:34:38 +0530

    divisible by 13 done

--- strings practice/Divisible_By_13.java
+++ strings practice/Divisible_By_13.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Divisible_By_13 {
     static boolean Solution(String s) {
-        return (Integer.parseInt(s) % 13 == 0);
+        int temp = 0;
+        for (int i = 0; i < s.length(); i++) {
+            temp = (temp * 10 + (s.charAt(i) - '0')) % 13;
+        }
+        return temp == 0;
     }
 
     public static void main(String[] args) {
