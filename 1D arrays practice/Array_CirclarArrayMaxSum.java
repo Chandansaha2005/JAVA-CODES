3036f2f574c34735d366045ffa2a874415d63156
Chandan Saha <chandansaha1945@gmail.com>
2025-07-25 07:48:45 +0530

    gfg problem

---
+++ 1D arrays practice/Array_CirclarArrayMaxSum.java
@@ -0,0 +1,24 @@
+public class Array_CirclarArrayMaxSum {
+    static int circularSubArraySum(int[] arr) {
+        int total = 0, tempMax = 0, tempMin = 0, maxSum = arr[0], minSum = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            tempMax = Math.max(tempMax + arr[i], tempMax);
+            maxSum = Math.max(maxSum, tempMax);
+
+            tempMin = Math.min(tempMin + arr[i], tempMin);
+            minSum = Math.min(minSum, tempMin);
+
+            total += arr[i];
+        }
+        int normSum = maxSum;
+        int circulerSum = total - minSum;
+        if (minSum == total)
+            return normSum;
+        return Math.max(normSum, circulerSum);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, -8, 9, -9, 10, 11, 12 };
+        circularSubArraySum(arr);
+    }
+}
