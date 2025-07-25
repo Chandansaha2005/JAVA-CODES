0c248db71bb486d8e580e090d7b3d4f6324d2e4d
Chandan Saha <chandansaha1945@gmail.com>
2025-07-25 07:50:50 +0530

    find the maximun sum in circular subarray

--- 1D arrays practice/Array_CirclarArrayMaxSum.java
+++ 1D arrays practice/Array_CirclarArrayMaxSum.java
@@ -2,10 +2,10 @@ public class Array_CirclarArrayMaxSum {
     static int circularSubArraySum(int[] arr) {
         int total = 0, tempMax = 0, tempMin = 0, maxSum = arr[0], minSum = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            tempMax = Math.max(tempMax + arr[i], tempMax);
+            tempMax = Math.max(tempMax + arr[i], arr[i]);
             maxSum = Math.max(maxSum, tempMax);
 
-            tempMin = Math.min(tempMin + arr[i], tempMin);
+            tempMin = Math.min(tempMin + arr[i], arr[i]);
             minSum = Math.min(minSum, tempMin);
 
             total += arr[i];
@@ -18,7 +18,8 @@ public class Array_CirclarArrayMaxSum {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 8, -8, 9, -9, 10, 11, 12 };
-        circularSubArraySum(arr);
+        int arr[] = { 8, -8, 9, -9, 10, -11, 12 };
+        System.out.println(circularSubArraySum(arr));
+        ;
     }
 }
