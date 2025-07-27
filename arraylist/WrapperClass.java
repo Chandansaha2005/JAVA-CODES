0f1536d3c9be726893c03a1c0b3926c46b2e10ed
Chandan Saha <chandansaha1945@gmail.com>
2025-07-27 17:18:51 +0530

    differrent type of wrapper classes

--- arraylist/WrapperClass.java
+++ arraylist/WrapperClass.java
@@ -2,5 +2,13 @@ public class WrapperClass {
     public static void main(String[] args) {
         Integer a = Integer.valueOf(3);
         System.out.println(a);
+        Float f = Float.valueOf(8);
+        System.out.println(f);
+        Boolean b = Boolean.valueOf(true);
+        System.out.println(b);
+        Long l = Long.valueOf(4);
+        System.out.println(l);
+        Character ch = Character.valueOf('s');
+        System.out.println(ch);
     }
 }
