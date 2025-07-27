fdb2108895a994abffc98585075100ee8a831b78
Chandan Saha <chandansaha1945@gmail.com>
2025-07-27 18:59:24 +0530

    arraylist without data type

---
+++ arraylist/Arryalist_diff.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Arryalist_diff {
+    public static void main(String[] args) {
+        ArrayList li = new ArrayList();
+        li.add('p');
+        li.add(2);
+        System.out.println(li);
+    }
+}
