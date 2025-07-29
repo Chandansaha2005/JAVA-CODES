8d6bee98726f4b51da00792321ab825d6f3d07d2
Chandan Saha <chandansaha1945@gmail.com>
2025-07-29 13:17:40 +0530

    done

--- 1D arrays practice/Array_SubArraySum2.java
+++ 1D arrays practice/Array_SubArraySum2.java
@@ -6,7 +6,6 @@ public class Array_SubArraySum2 {
         }
         return sum;
     }
-
     public static void main(String[] args) {
         int arr[] = { 1, 4, 5, 3, 2 };
         System.out.println(subArraySum(arr));
