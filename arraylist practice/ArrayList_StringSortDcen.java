25a2ddc9420a060adb09550bdc2061ba88831a16
Chandan Saha <chandansaha1945@gmail.com>
2025-07-30 15:39:05 +0530

    array list practice

--- arraylist practice/ArrayList_StringSortDcen.java
+++ arraylist practice/ArrayList_StringSortDcen.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class ArrayList_StringSortDcen {
-    
+    public static void main(String[] args) {
+        ArrayList<String> s = new ArrayList<>();
+        s.add("my name is chandan");
+        System.out.println(s);
+    }
 }
