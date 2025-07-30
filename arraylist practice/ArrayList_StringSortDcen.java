1929be49743b9c8a4293c282d90f09bad6450cb6
Chandan Saha <chandansaha1945@gmail.com>
2025-07-30 15:53:34 +0530

    array class vdo done

--- arraylist practice/ArrayList_StringSortDcen.java
+++ arraylist practice/ArrayList_StringSortDcen.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayList_StringSortDcen {
     public static void main(String[] args) {
         ArrayList<String> s = new ArrayList<>();
-        s.add("my name is chandan");
+        s.add("my");
+        s.add("name");
+        s.add("is");
+        s.add("chandan");
+        System.out.println(s);
+        Collections.sort(s);
+        Collections.reverse(s);
         System.out.println(s);
     }
 }
+
