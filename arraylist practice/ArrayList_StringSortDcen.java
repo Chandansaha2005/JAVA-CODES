4027960284ce2c6401dfbea662762f6e4c202877
Chandan Saha <chandansaha1945@gmail.com>
2025-08-03 23:12:58 +0530

    done

--- arraylist practice/ArrayList_StringSortDcen.java
+++ arraylist practice/ArrayList_StringSortDcen.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class ArrayList_StringSortDcen {
     public static void main(String[] args) {
         ArrayList<String> s = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        int[][] a = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
         s.add("my");
         s.add("name");
         s.add("is");
@@ -12,6 +15,10 @@ public class ArrayList_StringSortDcen {
         Collections.sort(s);
         Collections.reverse(s);
         System.out.println(s);
+        for (int[] i : a) {
+            for (int js : i) {
+                arr.add(js);
+            }
+        }
     }
 }
-
