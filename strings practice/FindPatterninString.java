6c230f92ba9b7755965d382fb9328f4e3c8e32de
Chandan Saha <chandansaha1945@gmail.com>
2025-08-03 16:23:08 +0530

    gfg qns solve

---
+++ strings practice/FindPatterninString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FindPatterninString {
+    static int[] patternFind(String s ,String pat){
+        int res[]=new int[s.length()];
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(minChar(sc.next()));
+        sc.close();
+    }
+}
--- strings practice/MinCharsAddForPalindrome.java
+++ strings practice/MinCharsAddForPalindrome.java
@@ -7,18 +7,10 @@ public class MinCharsAddForPalindrome {
         lps[0] = 0;
         int len = 0;
         while (i < n) {
-            if (s.charAt(i) == s.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len != 0)
-                    len = lps[len - 1];
-                else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
+            if (s.charAt(i) == s.charAt(len)) lps[i++] = ++len;
+            else 
+                if (len != 0) len = lps[len - 1];
+                else lps[i++] = 0;
         }
         return lps;
     }
