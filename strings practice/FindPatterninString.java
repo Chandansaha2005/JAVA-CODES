6ca58f14b38ba66b7a676cbbab225febafd6c3f3
Chandan Saha <chandansaha1945@gmail.com>
2025-08-03 18:01:02 +0530

    done but gfg bc

--- strings practice/FindPatterninString.java
+++ strings practice/FindPatterninString.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FindPatterninString {
-    static int[] patternFind(String s ,String pat){
-        int res[]=new int[s.length()];
+    static ArrayList<Integer> patternFind(String pat, String txt) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int l = txt.length();
+        for (int i = 0; i <= pat.length() - l; i++) {
+            if (txt.equals(pat.substring(i, i + l))) {
+                res.add(i);
+            }
+        }
         return res;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(minChar(sc.next()));
+        System.out.println(patternFind(sc.next(), sc.next()));
         sc.close();
     }
 }
