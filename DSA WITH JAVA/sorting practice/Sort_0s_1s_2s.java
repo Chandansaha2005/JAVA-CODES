43e5776bd01e065d3b95e0612c74946ad6aebde7
Chandan Saha <chandansaha1945@gmail.com>
2025-08-04 21:59:25 +0530

    with dnf algo

--- DSA WITH JAVA/sorting practice/Sort_0s_1s_2s.java
+++ DSA WITH JAVA/sorting practice/Sort_0s_1s_2s.java
@@ -5,7 +5,6 @@ public class Sort_0s_1s_2s {
     // Arrays.sort(arr);
     // }//brute force O(nlogn)
 
-    
     // static void sort012(int[] arr) {
     // int c0 = 0, c1 = 0, c2 = 0;
     // for (int i : arr) {
@@ -30,7 +29,23 @@ public class Sort_0s_1s_2s {
     // }
     // }//Optimized Approch
 
-   
+    static void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low++] = arr[mid];
+                arr[mid++] = temp;
+            } else if (arr[mid] == 1)
+                mid++;
+            else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high--] = temp;
+            }
+        }
+    }// with DNF algorithm
+
     public static void main(String[] args) {
         int[] arr = { 1, 0, 1, 0, 2, 0, 1, 2, 1, 2, 0, 0, 1, 0, 1 };
         sort012(arr);
