c81dfd119ac208b9365ede2f37bdacf5a1d25760
Chandan Saha <chandansaha1945@gmail.com>
2025-08-04 21:59:10 +0530

    with good time complex

--- DSA WITH JAVA/sorting practice/Sort_0s_1s_2s.java
+++ DSA WITH JAVA/sorting practice/Sort_0s_1s_2s.java
@@ -5,7 +5,32 @@ public class Sort_0s_1s_2s {
     // Arrays.sort(arr);
     // }//brute force O(nlogn)
 
+    
+    // static void sort012(int[] arr) {
+    // int c0 = 0, c1 = 0, c2 = 0;
+    // for (int i : arr) {
+    // if (i == 0)
+    // c0++;
+    // if (i == 1)
+    // c1++;
+    // if (i == 2)
+    // c2++;
+    // if (i > 2 || i < 0)
+    // return;
+    // }
+    // int in = 0;
+    // for (int i = 0; i < c0; i++) {
+    // arr[in++] = 0;
+    // }
+    // for (int i = 0; i < c1; i++) {
+    // arr[in++] = 1;
+    // }
+    // for (int i = 0; i < c2; i++) {
+    // arr[in++] = 2;
+    // }
+    // }//Optimized Approch
 
+   
     public static void main(String[] args) {
         int[] arr = { 1, 0, 1, 0, 2, 0, 1, 2, 1, 2, 0, 0, 1, 0, 1 };
         sort012(arr);
