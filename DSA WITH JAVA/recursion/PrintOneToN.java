146871c77abd108a47028e8631241bc74d409e7f
Chandan Saha <chandansaha1945@gmail.com>
2025-08-10 14:44:30 +0530

    printing 1 to n

--- DSA WITH JAVA/recursion/PrintOneToN.java
+++ DSA WITH JAVA/recursion/PrintOneToN.java
@@ -1,9 +1,9 @@
 public class PrintOneToN {
     static void pi(int n) {
-        if (n == 0)
-            return;
-        pi(n-1);
-        System.err.print(n + " ");
+        // if (n == 0)// Base Case
+        //     return;
+        pi(n - 1);// Assumption
+        System.err.print(n + " ");// SelfWork
     }
 
     public static void main(String[] args) {
