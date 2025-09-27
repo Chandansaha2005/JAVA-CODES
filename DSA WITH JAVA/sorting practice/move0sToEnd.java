edf1ec64048dd6ef407de36c2513ecc2e8798389
Chandan Saha <chandansaha1945@gmail.com>
2025-09-27 13:29:40 +0530

    sovling sorting problem

---
+++ DSA WITH JAVA/sorting practice/move0sToEnd.java
@@ -0,0 +1,13 @@
+public class move0sToEnd {
+    static void sort0s(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 0, 1, 0, 2, 0, 1, 2, 1, 2, 0, 0, 1, 0, 1 };
+        sort0s(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
