49b29d50fc385a25b67357753d445347285b148e
Chandan Saha <chandansaha1945@gmail.com>
2025-09-27 15:52:54 +0530

    move all zeros at the end problem

--- DSA WITH JAVA/sorting practice/move0sToEnd.java
+++ DSA WITH JAVA/sorting practice/move0sToEnd.java
@@ -1,10 +1,16 @@
 public class move0sToEnd {
     static void sort0s(int[] arr) {
-
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] == 0 && arr[j + 1] != 0) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 0, 1, 0, 2, 0, 1, 2, 1, 2, 0, 0, 1, 0, 1 };
+        int[] arr = { 0, 5, 0, 3, 4, 2 };
         sort0s(arr);
         for (int i : arr) {
             System.out.print(i + " ");
