248b4fb36e486447577e5f52221684d42ec25028
Chandan Saha <chandansaha1945@gmail.com>
2025-10-28 21:15:44 +0530

    starting againn

--- DSA WITH JAVA/sorting/InsertionSort.java
+++ DSA WITH JAVA/sorting/InsertionSort.java
@@ -1,13 +1,23 @@
+/**
+ * The InsertionSort class contains a static method that implements the
+ * insertion sort algorithm to
+ * sort an integer array in ascending order.
+ */
 
 public class InsertionSort {
     static void insertion(int arr[]) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
+            // This part of the code is the inner loop of the insertion sort algorithm. It
+            // is
+            // responsible for shifting elements to the right until the correct position for
+            // the current
+            // element is found within the sorted part of the array.
             for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
                 int temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
-            }
+            } f
         }
     }
 
@@ -25,7 +35,3 @@ public class InsertionSort {
         printArray(arr);
     }
 }
-
-
-
-
