b73b9f33156d7824a012ce5a928d36a0554cc4d6
Chandan Saha <chandansaha1945@gmail.com>
2025-11-22 01:18:26 +0530

    quick sort

--- DSA WITH JAVA/sorting/quickSorting.java
+++ DSA WITH JAVA/sorting/quickSorting.java
@@ -1,11 +1,37 @@
 import java.util.Scanner;
 
 public class quickSorting {
-    
+    static int partition(int arr[], int left, int right) {
+        int pivot = arr[right],i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[right];
+        arr[right] = temp;
+
+        return i;
+    }
+
+    static void quickSort(int arr[], int left, int right) {
+        if (left >= right) return;
+
+        int pivot = partition(arr, left, right);
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+
     static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
         System.out.println();
     }
 
@@ -14,15 +40,20 @@ public class quickSorting {
         System.out.print("Enter the number of elements = ");
         int n = sc.nextInt();
         int arr[] = new int[n];
+
         System.out.print("Enter elements = ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
         System.out.print("Before Sorting : ");
         printArray(arr);
-        mergeSort(arr, 0, arr.length - 1);
+
+        quickSort(arr, 0, arr.length - 1);
+
         System.out.print("After Sorting : ");
         printArray(arr);
+
         sc.close();
     }
 }
