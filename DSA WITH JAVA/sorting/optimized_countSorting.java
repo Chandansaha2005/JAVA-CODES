28b2c8b264c51b8d47db129c115c0cf39fb1f05b
Chandan Saha <chandansaha1945@gmail.com>
2025-11-23 21:31:37 +0530

    adjustments

--- DSA WITH JAVA/sorting/optimized_countSorting.java
+++ DSA WITH JAVA/sorting/optimized_countSorting.java
@@ -4,30 +4,24 @@ public class optimized_countSorting {
 
     static int findMax(int[] arr) {
         int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > max)
                 max = arr[i];
-            }
-        }
         return max;
     }
 
     static void countSort(int arr[]) {
         int max = findMax(arr);
         int[] count = new int[max + 1];
-
         for (int i = 0; i < arr.length; i++)
             count[arr[i]]++;
-
         for (int i = 1; i < count.length; i++)
             count[i] += count[i - 1];
-
         int[] res = new int[arr.length];
         for (int i = arr.length - 1; i >= 0; i--) {
             res[count[arr[i]] - 1] = arr[i];
             count[arr[i]]--;
         }
-
         for (int i = 0; i < res.length; i++)
             arr[i] = res[i];
     }
