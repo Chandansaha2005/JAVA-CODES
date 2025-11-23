9774e1db7df11d044c9d2c9106eea98e9dd099f0
Chandan Saha <chandansaha1945@gmail.com>
2025-11-23 12:49:32 +0530

    trying overlappingIntevals

---
+++ DSA WITH JAVA/sorting practice/overlappingIntevals.java
@@ -0,0 +1,10 @@
+public class overlappingIntevals {
+    
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 1, 3, 5 };
+        System.out.println(count(arr));
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
