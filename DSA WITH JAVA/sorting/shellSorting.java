c6a3a44e7245fb1b306ec727f6e1b946f24c858d
Chandan Saha <chandansaha1945@gmail.com>
2025-11-24 14:35:06 +0530

    shell sort done

--- DSA WITH JAVA/sorting/shellSorting.java
+++ DSA WITH JAVA/sorting/shellSorting.java
@@ -1,3 +1,56 @@
+import java.util.Scanner;
+
 public class shellSorting {
-    
+
+    static void shellSort(int arr[]) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int j = gap; j < arr.length; j++) {
+                for (int i = j - gap; i >= 0; i -= gap) {
+                    if (arr[i + gap] < arr[i]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + gap];
+                        arr[i + gap] = temp;
+                    } else
+                        break;
+                }
+            }
+        }
+        for (int gap = n / 2; gap > 0; gap /= 2)
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i], j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = temp;
+            }
+
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        shellSort(arr);
+        System.out.println("\nSorted array:");
+        printArray(arr);
+        sc.close();
+    }
+
 }
