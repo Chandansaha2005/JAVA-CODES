e310dd3d867386991fc35f361b4908ee1c8efcb6
Chandan Saha <chandansaha1945@gmail.com>
2025-11-24 16:54:57 +0530

    next problem on sorting

---
+++ DSA WITH JAVA/sorting practice/negetiveFollowedPositive.java
@@ -0,0 +1,13 @@
+public class negetiveFollowedPositive {
+    static void partition(int[] arr) {
+                
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, -2, -3, 8, -5, 6, 7, -4, 9 };
+        partition(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
