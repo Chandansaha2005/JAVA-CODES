165def5edbd7884f985af1599375da20125049ba
Chandan Saha <chandansaha1945@gmail.com>
2025-11-24 21:14:21 +0530

    insertIntervals done

--- DSA WITH JAVA/sorting practice/insertIntervals.java
+++ DSA WITH JAVA/sorting practice/insertIntervals.java
@@ -1,31 +1,46 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class insertIntervals {
     static int[][] insert(int[][] intervals, int[] newInterval) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         ArrayList<int[]> res = new ArrayList<>();
-        res.add(new int[] { intervals[0][0], intervals[0][1] });
-        for (int i = 1; i < intervals.length; i++) {
-            int[] last = res.get(res.size() - 1);
-            if (newInterval[0] <= last[1]) {
-                last[1] = Math.max(last[1], newInterval[1]);
-            } else {
-                res.add(new int[] { newInterval[0], newInterval[1] });
-            }
+        int i = 0;
+        int n = intervals.length;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        while (i < n) {
+            res.add(intervals[i]);
+            i++;
+        }
+
         return res.toArray(new int[res.size()][]);
     }
 
-    public static void main(String[] args) {
-        int[][] arr = { { 1, 3 }, { 5, 8 } };
-        int[] newarr = { 2, 4 };
-        int[][] insertd = insert(arr, newarr);
-        for (int[] interval : insertd) {
-            for (int val : interval) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
+    public static void printIntervals(int[][] intervals) {
+        for (int[] interval : intervals) {
+            System.out.print("[" + interval[0] + "," + interval[1] + "] ");
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = { { 1, 3 }, { 6, 9 } };
+        int[] newInterval = { 2, 5 };
+
+        int[][] result = insert(intervals, newInterval);
+
+        System.out.println("Merged intervals:");
+        printIntervals(result);
     }
 }
