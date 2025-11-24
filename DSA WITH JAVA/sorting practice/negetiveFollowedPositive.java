6d3832880d35fa0fd57577cef5751be058c32fe7
Chandan Saha <chandansaha1945@gmail.com>
2025-11-24 17:05:38 +0530

    trying another approch

--- DSA WITH JAVA/sorting practice/negetiveFollowedPositive.java
+++ DSA WITH JAVA/sorting practice/negetiveFollowedPositive.java
@@ -1,6 +1,22 @@
 public class negetiveFollowedPositive {
+    static swap(int i,int j){
+        int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+    }
+
     static void partition(int[] arr) {
-                
+        int pivot = arr[arr.length - 1], i = -1;
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
     }
 
     public static void main(String[] args) {
