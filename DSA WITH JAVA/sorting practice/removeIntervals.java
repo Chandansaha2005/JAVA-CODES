a54c84cc803a9d65d62c6b8f7b1698b300b8ff3a
Chandan Saha <chandansaha1945@gmail.com>
2025-11-25 14:46:11 +0530

    leetcode 435 remove intervals

---
+++ DSA WITH JAVA/sorting practice/removeIntervals.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class removeIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int count = 0, last = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < last)
+                count++;
+            else
+                last = intervals[i][1];
+        }
+        return count;
+    }
+
+    public void display(int result) {
+        System.out.println("Number of intervals to remove: " + result);
+    }
+
+    public static void main(String[] args) {
+        IntervalEraser eraser = new IntervalEraser();
+        int[][] intervals = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 1, 3 } };
+        int result = eraser.eraseOverlapIntervals(intervals);
+        eraser.display(result);
+    }
+}
