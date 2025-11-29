bd8a0d11b0e4c26d9631e7f513b1c8e027e4ba01
Chandan Saha <chandansaha1945@gmail.com>
2025-11-29 20:56:09 +0530

    builtheap

--- DSA WITH JAVA/sorting/maxheap.java
+++ DSA WITH JAVA/sorting/maxheap.java
@@ -1,5 +1,19 @@
 public class maxheap {
+    static void findMax(int[] arr, int index, int n) {
+        int largest=
+    }
+
+    static void builtheap(int[] arr, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            findMax(arr, i, n);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 9, 4, 3, 8, 10, 2, 5 };
+        builtheap(arr, arr.length);
+        for (int i : arr) {
+            System.out.print(i);
+        }
     }
 }
