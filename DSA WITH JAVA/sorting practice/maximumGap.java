cc43fdbdc71a67f336f9f3dc96536aa440d7ac78
Chandan Saha <chandansaha1945@gmail.com>
2025-11-29 15:23:01 +0530

    done

--- DSA WITH JAVA/sorting practice/maximumGap.java
+++ DSA WITH JAVA/sorting practice/maximumGap.java
@@ -1,13 +1,25 @@
+import java.util.Arrays;
+
 public class maximumGap {
     static int maxGap(int[] nums) {
-
+        int n = nums.length, max = 0;
+        if (n == 1)
+            return 0;
+        else {
+            Arrays.sort(nums);
+            for (int i = 0; i < n - 1; i++) {
+                max = Math.max(max, (nums[i + 1] - nums[i]));
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 5, 3, 6, 9 };
-        maxGap(arr);
+        int ans = maxGap(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
+        System.out.println("Maximum Gap = " + ans);
     }
 }
