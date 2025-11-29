0bf066bdc3ff5e039fc81cdfe0e9633f2e40ab2d
Chandan Saha <chandansaha1945@gmail.com>
2025-11-29 15:16:19 +0530

    leetcode 164

---
+++ DSA WITH JAVA/sorting practice/maximumGap.java
@@ -0,0 +1,10 @@
+public class maximumGap {
+    
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 3, 6, 9 };
+        maxGap(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
