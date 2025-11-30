1c197c82842959d2bb2bc5cccb94f64fe1f20d7a
Chandan Saha <chandansaha1945@gmail.com>
2025-11-30 12:43:00 +0530

    leetcode 493

--- DSA WITH JAVA/sorting practice/reversePairs.java
+++ DSA WITH JAVA/sorting practice/reversePairs.java
@@ -1,38 +1,32 @@
-class Solution {
+public class reversePairs {
     static int res = 0;
 
     static int merge(int[] arr, int l, int m, int r) {
         int[] temp = new int[r - l + 1];
         int i = l, j = m + 1, k = 0;
-
-        while (i <= m && j <= r) {
-            if (arr[i] > ( 2 * arr[j])) {
+        while (i <= m && j <= r)
+            if (arr[i] > (2L * arr[j])) {
                 res += (m - i + 1);
                 j++;
             } else
                 i++;
-        }
-        i = l; j = m + 1;
-        while (i <= m && j <= r) {
-            if (arr[i] <= arr[j]) {
+        i = l;
+        j = m + 1;
+        while (i <= m && j <= r)
+            if (arr[i] <= arr[j])
                 temp[k++] = arr[i++];
-            } else {
+            else
                 temp[k++] = arr[j++];
-            }
-        }
         while (i <= m)
             temp[k++] = arr[i++];
         while (j <= r)
             temp[k++] = arr[j++];
-
-        for (i = 0; i < temp.length; i++) {
+        for (i = 0; i < temp.length; i++)
             arr[l + i] = temp[i];
-        }
         return res;
     }
 
     static int mergeSort(int[] arr, int l, int r) {
-
         if (l < r) {
             int m = (r + l) / 2;
             mergeSort(arr, l, m);
@@ -42,8 +36,14 @@ class Solution {
         return res;
     }
 
-    public int reversePairs(int[] nums) {
+    public static int reversePairs(int[] nums) {
+        res = 0;
         return mergeSort(nums, 0, nums.length - 1);
     }
 
+    public static void main(String[] args) {
+        int[] nums = { 2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647 };
+        int result = reversePairs(nums);
+        System.out.println("Number of reverse pairs: " + result);
+    }
 }
