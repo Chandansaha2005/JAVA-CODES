97a2ef05cfa2f042900fe5678f2aadb701fb52e5
Chandan Saha <chandansaha1945@gmail.com>
2025-11-30 13:08:16 +0530

    cycleSorting

---
+++ DSA WITH JAVA/sorting/cycleSorting.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class cycleSorting {
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        shellSort(arr);
+        System.out.println("\nSorted array:");
+        printArray(arr);
+        sc.close();
+    }
+}
