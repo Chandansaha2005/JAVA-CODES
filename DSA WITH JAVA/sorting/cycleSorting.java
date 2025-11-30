7361941a082aff04c9bf0ee32950a1c4c9822c42
Chandan Saha <chandansaha1945@gmail.com>
2025-11-30 13:11:30 +0530

    cycle sort done

--- DSA WITH JAVA/sorting/cycleSorting.java
+++ DSA WITH JAVA/sorting/cycleSorting.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class cycleSorting {
 
+    static void cycleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == i)
+                i++;
+            else {
+                int idx = arr[i];
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
+            }
+        }
+    }
+
     static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
@@ -23,7 +36,7 @@ public class cycleSorting {
         }
         System.out.println("Original array:");
         printArray(arr);
-        shellSort(arr);
+        cycleSort(arr);
         System.out.println("\nSorted array:");
         printArray(arr);
         sc.close();
