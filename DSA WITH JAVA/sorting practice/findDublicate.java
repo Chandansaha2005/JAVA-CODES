51efe960d9d8e811a76786fd9607628159d350c4
Chandan Saha <chandansaha1945@gmail.com>
2025-11-30 14:08:16 +0530

    leetcode 287

--- DSA WITH JAVA/sorting practice/findDublicate.java
+++ DSA WITH JAVA/sorting practice/findDublicate.java
@@ -1,7 +1,8 @@
 public class findDublicate {
-    static int find(int[] arr) {
+    static int find(int[] nums) {
         for (int i = 0; i < nums.length;) {
             int idx = nums[i];
+            if(nums[i] == nums[idx])return nums[i];
             if (idx < nums.length && nums[i] != nums[idx]) {
                 int temp = nums[i];
                 nums[i] = nums[idx];
@@ -9,14 +10,11 @@ public class findDublicate {
             } else
                 i++;
         }
-        for (int i = 0; i < nums.length - 1; i++)
-            if (nums[i] == nums[i + 1])
-                return i;
         return 0;
     }
 
     public static void main(String[] args) {
-        int[] nums3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
-        System.out.println(missingNumber(nums3));
+        int[] nums3 = { 1, 3, 4, 2, 2 };
+        System.out.println(find(nums3));
     }
 }
