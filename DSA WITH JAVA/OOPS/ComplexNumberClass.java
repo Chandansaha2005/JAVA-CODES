2f3866db3ea106fa6af991100b81c00d6eeb1f32
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 12:27:45 +0530

    ComplexNumber done

--- DSA WITH JAVA/OOPS/ComplexNumberClass.java
+++ DSA WITH JAVA/OOPS/ComplexNumberClass.java
@@ -1,8 +1,7 @@
 class ComplexNumber {
     double x = 0, y = 0;
 
-    ComplexNumber() {
-    }
+    ComplexNumber() {}
 
     ComplexNumber(double x, double y) {
         this.x = x;
@@ -10,10 +9,10 @@ class ComplexNumber {
     }
 
     void print() {
-        if (y > 0)
+        if (y >= 0)
             System.out.print("\n" + this.x + "+" + this.y + "i");
         else
-            System.out.print("\n" + this.x + "-" + (-this.y) + "i");
+            System.out.print("\n" + this.x + "" + this.y + "i");
     }
 
     void add(ComplexNumber z) {
@@ -27,20 +26,28 @@ class ComplexNumber {
     }
 
     void mul(ComplexNumber z) {
-        x = x * z.x - y * z.y;
-        y = x * z.y + z.x * y;
+        double tempX = x * z.x - y * z.y;
+        double tempY = x * z.y + y * z.x;
+        x = tempX;
+        y = tempY;
     }
 
+    void divide(ComplexNumber z) {
+        double denominator = z.x * z.x + z.y * z.y;
+        double tempX = (x * z.x + y * z.y) / denominator;
+        double tempY = (y * z.x - x * z.y) / denominator;
+        x = tempX;
+        y = tempY;
+    }
 }
 
 public class ComplexNumberClass {
-
     public static void main(String[] args) {
         ComplexNumber cn1 = new ComplexNumber(5, 2);
-        ComplexNumber cn2 = new ComplexNumber(5, (-2));
+        ComplexNumber cn2 = new ComplexNumber(5, -2);
         cn1.print();
         cn2.print();
-        cn1.mul(cn2);
+        cn1.divide(cn2);
         cn1.print();
     }
 }
