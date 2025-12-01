f602add8b08826fb7749082b0858ef2a8904c95f
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 20:56:36 +0530

    finally created my own data structure

--- DSA WITH JAVA/OOPS/OwnArrayList.java
+++ DSA WITH JAVA/OOPS/OwnArrayList.java
@@ -10,6 +10,13 @@ public class OwnArrayList {
             arr = new int[n];
         }
 
+        void expandlist() {
+            int[] temp = new int[size * 2];
+            for (int i = 0; i < size; i++)
+                temp[i] = arr[i];
+            arr = temp;
+        }
+
         void add(int n) {
             if (size < arr.length)
                 arr[size++] = n;
@@ -19,18 +26,36 @@ public class OwnArrayList {
             }
         }
 
-        void expandlist() {
-            int[] temp = new int[size * 2];
-            for (int i = 0; i < size; i++) {
-                temp[i] = arr[i];
-            }
-            arr = temp;
-        }
-
         void print() {
             System.out.print("ArrayList:--- ");
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++)
                 System.out.print(arr[i] + " ");
+        }
+
+        void insert(int pos, int val) {
+            if (size < arr.length) {
+                for (int i = size + 1; i >= pos; i--)
+                    arr[i] = arr[i - 1];
+                arr[pos - 1] = val;
+                size++;
+            } else {
+                expandlist();
+                for (int i = size + 1; i >= pos; i--)
+                    arr[i] = arr[i - 1];
+                arr[pos - 1] = val;
+                size++;
+            }
+        }
+
+        void remove(int val) {
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == val) {
+                    for (int j = i; j < size; j++) {
+                        arr[j] = arr[j + 1];
+                    }
+                    size--;
+                    break;
+                }
             }
         }
     }
@@ -43,12 +68,8 @@ public class OwnArrayList {
         list.add(40);
         list.add(50);
         list.add(60);
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        list.add(60);
+        list.insert(1, 70);
+        list.remove(30);
         System.out.println("List Size:--- " + list.size);
         list.print();
     }
