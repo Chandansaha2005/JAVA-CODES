39a1d9529b130c85fe7055d1ac35980c8860afc2
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 17:25:01 +0530

    leetcode 41

--- DSA WITH JAVA/sorting practice/smallestMissingPositive.java
+++ DSA WITH JAVA/sorting practice/smallestMissingPositive.java
@@ -1,16 +1,26 @@
 public class smallestMissingPositive {
     public static int firstMissingPositive(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[])
+        int n = nums.length;
+        for (int i = 0; i < nums.length;) {
+            if (nums[i] > n || nums[i] < 1 || nums[i] == i + 1 || nums[i] == nums[nums[i] - 1])
+                i++;
+            else {
+                int idx = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+            }
         }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return n+1;
     }
 
     public static void main(String[] args) {
         int[] nums = { 3, 4, -1, 1 };
-        int[] arr = findErrorNums(nums);
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
+        System.out.print(firstMissingPositive(nums));
         System.out.println();
     }
 }
