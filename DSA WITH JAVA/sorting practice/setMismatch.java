6efacead9c726472d80f460c726678f167f566d2
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 16:37:31 +0530

    leetcode 645

--- DSA WITH JAVA/sorting practice/setMismatch.java
+++ DSA WITH JAVA/sorting practice/setMismatch.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class setMismatch {
     public static int[] findErrorNums(int[] nums) {
-        int[] res = new int[nums.length];
-        for (int i = 0; i < res.length;) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length;) {
             int idx = nums[i] - 1;
             if (nums[i] == nums[idx] || nums[i] == i + 1)
                 i++;
@@ -13,12 +13,21 @@ public class setMismatch {
                 nums[idx] = temp;
             }
         }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1)
+                res[0] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1)
+                res[1] = i+1;
+        }
+        return res;
     }
-
+//2 3 3 4 5 6
     public static void main(String[] args) {
-        int[] nums = { 4, 4, 4, 4 };
-        int[] nums = findDisappearedNumbers(nums);
-        for (int val : ans) {
+        int[] nums = { 3, 2, 3, 4, 6, 5 };
+        int[] arr = findErrorNums(nums);
+        for (int val : arr) {
             System.out.print(val + " ");
         }
         System.out.println();
