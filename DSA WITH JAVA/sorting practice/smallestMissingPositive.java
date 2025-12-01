f975eee3fb076d1fd699a92ebacb3002aba9f8cc
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 20:18:16 +0530

    gfg

--- DSA WITH JAVA/sorting practice/smallestMissingPositive.java
+++ DSA WITH JAVA/sorting practice/smallestMissingPositive.java
@@ -1,26 +1,26 @@
 public class smallestMissingPositive {
-    public static int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < nums.length;) {
-            if (nums[i] > n || nums[i] < 1 || nums[i] == i + 1 || nums[i] == nums[nums[i] - 1])
+    public static int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < arr.length;) {
+            if (arr[i] > n || arr[i] < 1 || arr[i] == i + 1 || arr[i] == arr[arr[i] - 1])
                 i++;
             else {
-                int idx = nums[i] - 1;
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = temp;
+                int idx = arr[i] - 1;
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1)
                 return i + 1;
         }
         return n+1;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 3, 4, -1, 1 };
-        System.out.print(firstMissingPositive(nums));
+        int[] arr = { 3, 4, -1, 1 };
+        System.out.print(firstMissingPositive(arr));
         System.out.println();
     }
 }
