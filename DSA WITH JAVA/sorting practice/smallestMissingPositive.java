ca0f5e2b5713619709c4b053265c1526de6cf550
Chandan Saha <chandansaha1945@gmail.com>
2025-12-01 17:09:57 +0530

    smallest Missing Positive

---
+++ DSA WITH JAVA/sorting practice/smallestMissingPositive.java
@@ -0,0 +1,16 @@
+public class smallestMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[])
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, -1, 1 };
+        int[] arr = findErrorNums(nums);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
