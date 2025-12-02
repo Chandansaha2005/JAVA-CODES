172410f4be8f88fdd70d49b8e7dae468e01a2f2a
Chandan Saha <chandansaha1945@gmail.com>
2025-12-02 13:55:17 +0530

    DeleteAnodeInLinkedList

---
+++ DSA WITH JAVA/LinkedList practice/DeleteAnodeInLinkedList.java
@@ -0,0 +1,35 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class DeleteAnodeInLinkedList {
+    public static void deleteNode(ListNode node) {// Problem function
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        printList(head);
+        deleteNode(head.next.next);
+        printList(head);
+    }
+}
