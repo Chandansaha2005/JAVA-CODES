714d08c2af6dd4e1688e2fbcee9e298f22f9daf9
Chandan Saha <chandansaha1945@gmail.com>
2025-12-02 14:36:12 +0530

    leetcode 876

---
+++ DSA WITH JAVA/LinkedList practice/SlowFastMiddleOfList.java
@@ -0,0 +1,30 @@
+public class SlowFastMiddleOfList {
+    public static ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+
+    public static void printList(ListNode head) {
+        for (ListNode temp = head; temp != null; temp = temp.next) {
+            System.out.print(temp.val + " -> ");
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        printList(head);
+        ListNode mid = middleNode(head);
+        System.out.println("Middle node value: " + mid.val);
+    }
+}
