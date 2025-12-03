9318d633ea3915ffcdee5e120dd5ccc6148a19e4
Chandan Saha <chandansaha1945@gmail.com>
2025-12-03 12:34:18 +0530

    addAtTail

--- DSA WITH JAVA/LinkedList/CircularLinkedlist.java
+++ DSA WITH JAVA/LinkedList/CircularLinkedlist.java
@@ -1,10 +1,22 @@
 class CLinkedList {
     Node head;
-    int size;
+    Node tail;
+    int size = 0;
 
-    
+    void addAtTail(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = tail = newNode;
+            head.next = tail;
+            tail.next = head;
+        } else {
+            tail.next = newNode;
+            newNode.next = head;
+            tail = newNode;
+        }
+    }
 }
 
 public class CircularLinkedlist {
-
+    
 }
