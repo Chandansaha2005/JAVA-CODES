f402baec3b15308ac6769d1d3634cbd67a5888b1
Chandan Saha <chandansaha1945@gmail.com>
2025-12-03 16:55:42 +0530

    leetcode 160

--- DSA WITH JAVA/LinkedList practice/IntersectionOfTwoLinkedList.java
+++ DSA WITH JAVA/LinkedList practice/IntersectionOfTwoLinkedList.java
@@ -1,55 +1,53 @@
 public class IntersectionOfTwoLinkedList {
-    public static Node intersectPoint(Node head1, Node head2) {
+    public static ListNode intersectPoint(ListNode headA, ListNode headB) {
         int h1 = 0, h2 = 0;
-        for (Node temp = head1; temp != null; temp = temp.next)
+        for (ListNode temp = headA; temp != null; temp = temp.next)
             h1++;
-        for (Node temp = head2; temp != null; temp = temp.next)
+        for (ListNode temp = headB; temp != null; temp = temp.next)
             h2++;
-        Node start1 = head1;
-        Node start2 = head2;
-        if (h1 > h2) {
+        ListNode start1 = headA, start2 = headB;
+        if (h1 > h2)
             for (int i = 0; i < h1 - h2; i++)
                 start1 = start1.next;
-        } else if (h1 < h2) {
+        else if (h1 < h2)
             for (int i = 0; i < h2 - h1; i++)
                 start2 = start2.next;
-        }
-        for (; start1 != null; start1 = start1.next, start2 = start2.next) {
-
-        }
+        for (; start1 != null; start1 = start1.next, start2 = start2.next)
+            if (start1 == start2)
+                return start1;
         return null;
     }
 
-    public static void printList(Node head) {
-        Node curr = head;
+    public static void printList(ListNode head) {
+        ListNode curr = head;
         while (curr != null) {
-            System.out.print(curr.data + " ");
+            System.out.print(curr.val + " ");
             curr = curr.next;
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        Node common = new Node(15);
-        common.next = new Node(30);
+        ListNode common = new ListNode(15);
+        common.next = new ListNode(30);
 
         // First list: 10 -> 15 -> 30
-        Node head1 = new Node(10);
-        head1.next = common;
+        ListNode headA = new ListNode(10);
+        headA.next = common;
 
         // Second list: 3 -> 6 -> 9 -> 15 -> 30
-        Node head2 = new Node(3);
-        head2.next = new Node(6);
-        head2.next.next = new Node(9);
-        head2.next.next.next = common; // creating an intersection at node with value 3
+        ListNode headB = new ListNode(3);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(9);
+        headB.next.next.next = common; // creating an intersection at Listnode with value 3
 
         System.out.println("List 1:");
-        printList(head1);
+        printList(headA);
 
         System.out.println("List 2:");
-        printList(head2);
+        printList(headB);
 
-        Node intersection = intersectPoint(head1, head2);
-        System.out.println("Intersection point: " + (intersection != null ? intersection.data : "None"));
+        ListNode intersection = intersectPoint(headA, headB);
+        System.out.println("Intersection point: " + (intersection != null ? intersection.val : "None"));
     }
 }
