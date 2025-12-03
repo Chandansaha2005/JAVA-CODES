729764f59f478d648c2b5361af5318084a079cd2
Chandan Saha <chandansaha1945@gmail.com>
2025-12-03 13:27:52 +0530

    DeleteKthElementFromLast

---
+++ DSA WITH JAVA/LinkedList practice/DeleteKthElementFromLast.java
@@ -0,0 +1,40 @@
+public class DeleteKthElementFromLast {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        if (slow.next == null)
+            slow = prev;
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        printList(head);
+        printList(removeNthFromEnd(head, 1));
+    }
+}
