f2ba56729e1f7f604cfdd4b676367bc1712cea65
Chandan Saha <chandansaha1945@gmail.com>
2025-12-04 13:48:29 +0530

    DetectLinkedListCycle

---
+++ DSA WITH JAVA/LinkedList Problems/DetectLinkedListCycle.java
@@ -0,0 +1,28 @@
+public class DetectLinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null) {
+            if (slow == fast)
+                return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // Create a linked list: 3 -> 2 -> 0 -> -4
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+
+        // Create a cycle: -4 -> 2
+        head.next.next.next.next = head.next;
+
+        boolean result = hasCycle(head);
+
+        System.out.println("Cycle detected: " + result);
+    }
+}
