6ba29a934755be86581e5585e1caf8917b6a0d22
Chandan Saha <chandansaha1945@gmail.com>
2025-12-04 14:02:13 +0530

    another approch

--- DSA WITH JAVA/LinkedList Problems/DetectLinkedListCycle.java
+++ DSA WITH JAVA/LinkedList Problems/DetectLinkedListCycle.java
@@ -9,6 +9,16 @@ public class DetectLinkedListCycle {
                 return true;
         }
         return false;
+
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // while (fast != null && fast.next != null && fast.next.next != null) {
+        //     slow = slow.next;
+        //     fast = fast.next.next.next;
+        //     if (slow == fast)
+        //         return true;
+        // }
+        // return false;
     }
 
     public static void main(String[] args) {
