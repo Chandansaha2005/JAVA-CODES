4f1a3381b894dedd786f7d06f7961174da3eed03
Chandan Saha <chandansaha1945@gmail.com>
2025-12-06 06:53:47 +0530

    leetcode 148

--- DSA WITH JAVA/LinkedList Problems/SortLinkedList.java
+++ DSA WITH JAVA/LinkedList Problems/SortLinkedList.java
@@ -1,10 +1,10 @@
 public class SortLinkedList {
-    public static Node temp = new Node(0);
+    public static ListNode temp = new ListNode(0);
 
-    public static Node merge(Node head1, Node head2) {
-        Node k = temp, i = head1, j = head2;
+    public static ListNode merge(ListNode head1, ListNode head2) {
+        ListNode k = temp, i = head1, j = head2;
         while (i != null && j != null) {
-            if (i.data <= j.data) {
+            if (i.val <= j.val) {
                 k.next = i;
                 i = i.next;
                 k = k.next;
@@ -27,39 +27,39 @@ public class SortLinkedList {
         return temp.next;
     }
 
-    public static Node mergeSort(Node head) {
+    public static ListNode mergeSort(ListNode head) {
         if (head.next == null)
             return head;
-        Node slow = head, fast = head;
+        ListNode slow = head, fast = head;
         while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        Node head2 = slow.next;
+        ListNode head2 = slow.next;
         slow.next = null;
         head = mergeSort(head);
         head2 = mergeSort(head2);
         return merge(head, head2);
     }
 
-    public static void printList(Node head) {
-        Node curr = head;
+    public static void printList(ListNode head) {
+        ListNode curr = head;
         while (curr != null) {
-            System.out.print(curr.data + " ");
+            System.out.print(curr.val + " ");
             curr = curr.next;
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        Node head = new Node(4);
-        head.next = new Node(2);
-        head.next.next = new Node(1);
-        head.next.next.next = new Node(3);
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
 
         System.out.println("Original list:");
         printList(head);
-        Node result = mergeSort(head);
+        ListNode result = mergeSort(head);
 
         System.out.println("List after mergeSort call:");
         printList(result);
