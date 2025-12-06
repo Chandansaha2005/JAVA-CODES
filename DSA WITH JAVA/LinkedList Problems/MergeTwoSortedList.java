6fdf1b44d1c311975e51a6dec97a98b25552f5b0
Chandan Saha <chandansaha1945@gmail.com>
2025-12-06 06:01:04 +0530

    leetcode 21

--- DSA WITH JAVA/LinkedList Problems/MergeTwoSortedList.java
+++ DSA WITH JAVA/LinkedList Problems/MergeTwoSortedList.java
@@ -1,25 +1,48 @@
 public class MergeTwoSortedList {
-    public static Node sortedMerge(Node head1, Node head2) {
-        return null;
+    public static ListNode sortedMerge(ListNode head1, ListNode head2) {
+        ListNode temp = new ListNode(0);
+        ListNode k = temp, i = head1, j = head2;
+        while (i != null && j != null) {
+            if (i.val <= j.val) {
+                k.next = i;
+                i = i.next;
+                k = k.next;
+            } else {
+                k.next = j;
+                j = j.next;
+                k = k.next;
+            }
+        }
+        while (i != null) {
+            k.next = i;
+            i = i.next;
+            k = k.next;
+        }
+        while (j != null) {
+            k.next = j;
+            j = j.next;
+            k = k.next;
+        }
+        return temp.next;
     }
 
-    public static void printList(Node head) {
-        Node curr = head;
+    public static void printList(ListNode head) {
+        ListNode curr = head;
         while (curr != null) {
-            System.out.print(curr.data + " ");
+            System.out.print(curr.val + " ");
             curr = curr.next;
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        Node head1 = new Node(1);
-        head1.next = new Node(3);
-        head1.next.next = new Node(5);
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(3);
+        head1.next.next = new ListNode(5);
 
-        Node head2 = new Node(2);
-        head2.next = new Node(4);
-        head2.next.next = new Node(6);
+        ListNode head2 = new ListNode(2);
+        head2.next = new ListNode(4);
+        head2.next.next = new ListNode(6);
 
         System.out.println("List 1:");
         printList(head1);
@@ -27,11 +50,9 @@ public class MergeTwoSortedList {
         System.out.println("List 2:");
         printList(head2);
 
-        Node result = sortedMerge(head1, head2);
+        ListNode result = sortedMerge(head1, head2);
 
         System.out.println("List after sortedMerge call:");
         printList(result);
     }
 }
-
-
