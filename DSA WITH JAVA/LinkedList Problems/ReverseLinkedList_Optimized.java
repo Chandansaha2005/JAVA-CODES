ed7d8c6642123bb1e4c94f55a39b53f2d117c0e1
Chandan Saha <chandansaha1945@gmail.com>
2025-12-07 07:35:33 +0530

    ReverseLinkedList_Optimized

---
+++ DSA WITH JAVA/LinkedList Problems/ReverseLinkedList_Optimized.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class ReverseLinkedList_Optimized {
+    
+    public static Node reverseList(Node head) {
+        ArrayList<Node> rev = new ArrayList<>();
+        for (Node temp = head; temp != null; temp = temp.next)rev.add(temp);
+        for (int i = rev.size() - 1; i > 0; i--)rev.get(i).next = rev.get(i - 1);
+        rev.get(0).next = null;
+        return rev.get((rev.size()-1));
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        Node result = reverseList(head);
+
+        System.out.println("List after reverseList call:");
+        printList(result);
+    }
+
+}
