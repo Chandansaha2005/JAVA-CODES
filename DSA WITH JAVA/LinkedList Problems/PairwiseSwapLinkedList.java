bb0b686ce2b6a025755081b85fda35e81adbd75e
Chandan Saha <chandansaha1945@gmail.com>
2025-12-07 05:37:13 +0530

    PairwiseSwapLinkedList

---
+++ DSA WITH JAVA/LinkedList Problems/PairwiseSwapLinkedList.java
@@ -0,0 +1,31 @@
+public class PairwiseSwapLinkedList {
+    public static Node pairwiseSwap(Node head) {
+        return head;
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        Node result = pairwiseSwap(head);
+
+        System.out.println("List after pairwiseSwap call:");
+        printList(result);
+    }
+}
+
