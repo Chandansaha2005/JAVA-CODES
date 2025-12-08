768fd4cef53923f8cdbad118e2c073a56275196c
Chandan Saha <chandansaha1945@gmail.com>
2025-12-08 08:54:20 +0530

    Gfg ReorderLinkedList

--- DSA WITH JAVA/LinkedList Problems/ReorderLinkedList.java
+++ DSA WITH JAVA/LinkedList Problems/ReorderLinkedList.java
@@ -1,6 +1,29 @@
 public class ReorderLinkedList {
     public static void reorderlist(Node head) {
-        
+        // Partition
+        Node s = head, f = head;
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        // Reverse 2nd Half
+        Node a = s.next, b = s.next, c = null;
+        s.next = null;
+        while (b != null) {
+            b = a.next;
+            a.next = c;
+            c = a;
+            a = b;
+        }
+        // Merge With Head
+        Node i = head, j = i.next, k = c;
+        while (i != null && k != null) {
+            i.next = k;
+            k = k.next;
+            i.next.next = j;
+            i = j;
+            j = j.next;
+        }
     }
 
     public static void printList(Node head) {
@@ -18,6 +41,8 @@ public class ReorderLinkedList {
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(7);
 
         System.out.println("Original list:");
         printList(head);
