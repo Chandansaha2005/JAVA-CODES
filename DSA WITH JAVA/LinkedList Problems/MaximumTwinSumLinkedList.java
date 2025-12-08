37f593b012c2e14facd9c4a087496f0303e06eca
Chandan Saha <chandansaha1945@gmail.com>
2025-12-08 06:11:30 +0530

    MaximumTwinSumLinkedList

---
+++ DSA WITH JAVA/LinkedList Problems/MaximumTwinSumLinkedList.java
@@ -0,0 +1,65 @@
+public class MaximumTwinSumLinkedList {
+    public static int pairSum(ListNode head) {
+        ListNode s = head, f = head;
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        ListNode curr = s, fwd = s, prev = null;
+        while (fwd != null) {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        int max = 0;
+        while (head != null && prev != null) {
+            int sum = head.val + prev.val;
+            if (sum > max)
+                max = sum;
+            head = head.next;
+            prev = prev.next;
+        }
+        return max;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(5);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        int result = pairSum(head);
+
+        System.out.println("Maximum twin sum: " + result);
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
