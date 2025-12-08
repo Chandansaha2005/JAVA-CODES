59eb685e22a562ea688fc2f8e3f6dba1c94ff68a
Chandan Saha <chandansaha1945@gmail.com>
2025-12-08 07:22:44 +0530

    leetcode 24 Optimized

--- DSA WITH JAVA/LinkedList Problems/PairwiseSwapLinkedList_Optimized.java
+++ DSA WITH JAVA/LinkedList Problems/PairwiseSwapLinkedList_Optimized.java
@@ -2,39 +2,17 @@ public class PairwiseSwapLinkedList_Optimized {
     public static ListNode pairwiseSwap(ListNode head) {
         if (head == null || head.next == null)
             return head;
-        ListNode oddList = new ListNode(0), evenList = new ListNode(0);
-        ListNode odd = oddList, even = evenList;
-        ListNode curr = head;
-        int idx = 1;
-        while (curr != null) {
-            if (idx % 2 != 0) {
-                odd.next = curr;
-                odd = odd.next;
-            } else {
-                even.next = curr;
-                even = even.next;
-            }
-            curr = curr.next;
-            idx++;
-        }
-        odd.next = null;
-        even.next = null;
         ListNode res = new ListNode(0);
-        ListNode tail = res;
-        ListNode o = oddList.next, e = evenList.next;
-        while (o != null || e != null) {
-            if (e != null) {
-                tail.next = e;
-                tail = tail.next;
-                e = e.next;
-            }
-            if (o != null) {
-                tail.next = o;
-                tail = tail.next;
-                o = o.next;
-            }
+        ListNode a = head, b = head.next, c = res;
+        while (a != null && b != null) {
+            c.next = b;
+            a.next = b.next;
+            b.next = a;
+            c = a;
+            a = a.next;
+            if (a != null)
+                b = a.next;
         }
-        tail.next = null;
         return res.next;
     }
 
