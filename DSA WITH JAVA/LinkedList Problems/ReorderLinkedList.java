32ad8ce862113bc551a470eee95d8eb261f666bf
Chandan Saha <chandansaha1945@gmail.com>
2025-12-08 08:27:01 +0530

    ReorderLinkedList

---
+++ DSA WITH JAVA/LinkedList Problems/ReorderLinkedList.java
@@ -0,0 +1,30 @@
+public class ReorderLinkedList {
+    public static void reorderlist(Node head) {
+        
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        reorderlist(head);
+
+        System.out.println("List after reorderlist call:");
+        printList(head);
+    }
+}
