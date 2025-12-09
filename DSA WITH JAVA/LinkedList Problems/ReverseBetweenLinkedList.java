d0ee4f505081d58da5b67d016e1643466d2b387f
Chandan Saha <chandansaha1945@gmail.com>
2025-12-09 11:18:04 +0530

    leetcode 92

--- DSA WITH JAVA/LinkedList Problems/ReverseBetweenLinkedList.java
+++ DSA WITH JAVA/LinkedList Problems/ReverseBetweenLinkedList.java
@@ -1,10 +1,10 @@
 public class ReverseBetweenLinkedList {
-    public static Node reverseBetween(Node head, int left, int right) {
-        Node temp = head;
-        Node l1 = null;
-        Node l2 = null;
-        Node r1 = null;
-        Node r2 = null;
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode temp = head;
+        ListNode l1 = null;
+        ListNode l2 = null;
+        ListNode r1 = null;
+        ListNode r2 = null;
         if (left == 1) {
             l1 = temp;
             for (int i = 1; i <= right; i++) {
@@ -42,8 +42,8 @@ public class ReverseBetweenLinkedList {
         return head;
     }
 
-    public static Node reverseList(Node head) {
-        Node p = null, c = head, f = head;
+    public static ListNode reverseList(ListNode head) {
+        ListNode p = null, c = head, f = head;
         if (head == null || head.next == null)
             return head;
         while (f != null) {
@@ -55,10 +55,10 @@ public class ReverseBetweenLinkedList {
         return p;
     }
 
-    public static void printList(Node head) {
-        Node curr = head;
+    public static void printList(ListNode head) {
+        ListNode curr = head;
         while (curr != null) {
-            System.out.print(curr.data + " ");
+            System.out.print(curr.val + " ");
             curr = curr.next;
         }
         System.out.println();
@@ -66,35 +66,35 @@ public class ReverseBetweenLinkedList {
 
     public static void main(String[] args) {
         // Example list: 1 -> 2 -> 3 -> 4 -> 5
-        Node head = new Node(1,
-                new Node(2,
-                        new Node(3,
-                                new Node(4,
-                                        new Node(5)))));
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5)))));
 
         System.out.println("Original list:");
         printList(head);
 
-        Node result = reverseBetween(head, 2, 4);
+        ListNode result = reverseBetween(head, 2, 4);
 
         System.out.println("List after reverseBetween call:");
         printList(result);
     }
 }
 
-class Node {
-    int data;
-    Node next;
+class ListNode {
+    int val;
+    ListNode next;
 
-    Node() {
+    ListNode() {
     }
 
-    Node(int data) {
-        this.data = data;
+    ListNode(int val) {
+        this.val = val;
     }
 
-    Node(int data, Node next) {
-        this.data = data;
+    ListNode(int val, ListNode next) {
+        this.val = val;
         this.next = next;
     }
 }
