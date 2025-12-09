00aad92a09548924d1dbf3052974fc36e58a9368
Chandan Saha <chandansaha1945@gmail.com>
2025-12-09 21:37:14 +0530

    push

--- DSA WITH JAVA/Stack/StackUsingArray.java
+++ DSA WITH JAVA/Stack/StackUsingArray.java
@@ -1,3 +1,40 @@
+class StackClass {
+    int MAX = 5;
+    int[] stack = new int[MAX];
+    int top = -1;
+
+    void push(int data) {
+        if (top == MAX - 1)
+            System.out.println("Stack Overflow");
+        else
+            stack[++top] = data;
+    }
+
+    int pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return stack[top--];
+    }
+
+    int peek() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        return stack[top];
+    }
+
+    void printStack() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return;
+        }
+
+    }
+}
+
 public class StackUsingArray {
-    
+
 }
