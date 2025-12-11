27b126bc1b9f74070ad6876416476d97a253f302
Chandan Saha <chandansaha1945@gmail.com>
2025-12-11 12:53:26 +0530

    attempting

--- DSA WITH JAVA/LinkedList Problems/CriticalPointsInList.java
+++ DSA WITH JAVA/LinkedList Problems/CriticalPointsInList.java
@@ -1,17 +1,25 @@
-import java.util.ArrayList;
-
 public class CriticalPointsInList {
     public static int[] nodesBetweenCriticalPoints(ListNode head) {
         int[] arr = { -1, -1 };
-        ArrayList<ListNode> cp = new ArrayList<>();
+        int f = -1, l = -1, p = -1, idx = 1;
         if (head == null || head.next == null || head.next.next == null)
             return arr;
         ListNode a = head, b = head.next, c = head.next.next;
         while (c != null) {
-            if ((b.val > a.val && b.val > c.val) || (b.val < a.val && b.val < b.val))
-                cp.add(b);
+            if ((b.val > a.val && b.val > c.val) || (b.val < a.val && b.val < c.val))
+                if (f == -1)
+                    f = idx;
+                else {
+                    p = l;
+                    l = idx;
+                }
+            a = b;
+            b = c;
+            c = c.next;
+            idx++;
         }
-        System.err.println(cp);
+        arr[0] = l - p;
+        arr[1] = l - f;
         return arr;
     }
 
@@ -20,7 +28,16 @@ public class CriticalPointsInList {
                 new ListNode(3,
                         new ListNode(1,
                                 new ListNode(2,
-                                        new ListNode(5)))));
+                                        new ListNode(5,
+                                                new ListNode(1,
+                                                        new ListNode(2)))))));
+
+        ListNode p = head;
+        while (p != null) {
+            System.out.print(p.val + " ");
+            p = p.next;
+        }
+        System.out.println();
 
         int[] res = nodesBetweenCriticalPoints(head);
         for (int x : res)
