0ec19ad72e7090fed484207e1a00176b716e3ef5
Chandan Saha <chandansaha1945@gmail.com>
2025-12-12 13:02:47 +0530

    PushAtBottom

---
+++ DSA WITH JAVA/Stack Problems/PushAtBottom.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class PushAtBottom {
+    public static void PushAtEnd(Stack<Integer> st, int data) {
+        if (st.size() == 0) {
+            st.push(data);
+            return;
+        }
+        int val = st.pop();
+        PushAtEnd(st, data);
+        st.push(val);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        st.pop();
+        System.out.println("Original stack: " + st);
+
+        PushAtEnd(st, 7);
+
+        System.out.println("After reverseStack: " + st);
+    }
+}
