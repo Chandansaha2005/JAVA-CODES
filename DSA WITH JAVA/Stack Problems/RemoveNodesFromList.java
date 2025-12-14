b5958f586f1e719e11abf52d652e4262ff2426fa
Chandan Saha <chandansaha1945@gmail.com>
2025-12-14 18:47:55 +0530

    RemoveNodesFromList

---
+++ DSA WITH JAVA/Stack Problems/RemoveNodesFromList.java
@@ -0,0 +1,65 @@
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveNodesFromList {
+    public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> st = new Stack<>();
+        for (ListNode temp = head; temp != null; temp = temp.next) {
+            if (temp == head)
+                st.push(temp);
+            else if (st.peek().val < temp.val) {
+                while (!st.isEmpty() &&st.peek().val < temp.val)
+                    st.pop();
+                st.push(temp);
+            } else
+                st.push(temp);
+        }
+        ListNode newhead = new ListNode(0);
+        ListNode temp = newhead;
+        while (!st.isEmpty()) {
+            temp.next = st.pop();
+            temp = temp.next;
+        }
+        temp.next = null;
+        return newhead.next; 
+    }
+
+    
+
+    // utility to print list
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Example 1: [5,2,13,3,8]
+        ListNode head = new ListNode(5);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(13);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(8);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        RemoveNodesFromList sol = new RemoveNodesFromList();
+        ListNode result = sol.removeNodes(head);
+
+        System.out.println("Modified list:");
+        printList(result);
+    }
+}
