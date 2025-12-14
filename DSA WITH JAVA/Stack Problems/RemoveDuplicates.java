2d1ea64f2e84bb0349a9debb5c879bc0db799e89
Chandan Saha <chandansaha1945@gmail.com>
2025-12-14 16:31:53 +0530

    leetcode 1047

---
+++ DSA WITH JAVA/Stack Problems/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class RemoveDuplicates {
+    public static String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) 
+            if (!st.isEmpty() && st.peek() == ch)  st.pop();
+            else st.push(ch);
+        StringBuilder res = new StringBuilder();
+        for (char ch : st)  res.append(ch);
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] tests = { /* "a", "aa", "abca", "azxxzy", "abbaca", "abcabc", */"aabbcc"
+        };
+
+        for (String t : tests) {
+            String result = removeDuplicates(t);
+            System.out.println("Input: \"" + t + "\" -> Output: \"" + result + "\"");
+        }
+    }
+}
