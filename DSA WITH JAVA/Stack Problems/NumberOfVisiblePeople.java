dc023ccceff68098ccc20a1577ef27d691de74fb
Chandan Saha <chandansaha1945@gmail.com>
2025-12-18 13:08:40 +0530

    NumberOfVisiblePeople

--- DSA WITH JAVA/Stack Problems/NumberOfVisiblePeople.java
+++ DSA WITH JAVA/Stack Problems/NumberOfVisiblePeople.java
@@ -1,17 +1,32 @@
 import java.util.Arrays;
 
 public class NumberOfVisiblePeople {
-    public static int[] canSeePersonsCount(int[] heights) {
-        // placeholder
-        return new int[heights.length];
+    public static int[] canSeePersonsCount(int[] h) {
+        int[] arr = new int[h.length];
+        for (int i = 0; i < h.length - 1; i++) {
+            int count = 1;
+            int prev = 0;
+            for (int j = i + 1; h[i] > h[j] && j < h.length - 1;) {
+                prev = j;
+                j++;
+                if (h[prev] > h[j])
+                    continue;
+                count++;
+                if (h[i] < h[j])
+                    break;
+            }
+            arr[i] = count;
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
         int[][] tests = {
-            {10, 6, 8, 5, 11, 9},
-            {5, 1, 2, 3, 10},
-            {3, 2, 1},
-            {1, 2, 3, 4}
+                { 11, 19, 12, 15, 14, 18, 7, 1, 8, 9 },
+                { 10, 6, 8, 5, 11, 9 },
+                { 5, 1, 2, 3, 10 },
+                { 3, 2, 1 },
+                { 1, 2, 3, 4 }
         };
 
         for (int[] test : tests) {
