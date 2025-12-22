748127bc722985386e0f9d0181609d6544433129
Chandan Saha <chandansaha1945@gmail.com>
2025-12-22 11:26:06 +0530

    leetcode 1944

--- DSA WITH JAVA/Stack Problems/NumberOfVisiblePeople.java
+++ DSA WITH JAVA/Stack Problems/NumberOfVisiblePeople.java
@@ -6,17 +6,17 @@ public class NumberOfVisiblePeople {
         int n = h.length;
         int[] arr = new int[n];
         Stack<Integer> st = new Stack<>();
+        arr[n - 1] = 0;
         st.push(h[n - 1]);
-        for (int i = n - 2; i > 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {
             int count = 0;
-            while (!st.isEmpty() && st.peek() < h[i]) {
+            while (!st.isEmpty() && st.peek() <= h[i]) {
                 st.pop();
                 count++;
             }
-            if (h[i] < st.peek()) {
-                st.push(h[i]);
+            if (!st.isEmpty())
                 count++;
-            }
+            st.push(h[i]);
             arr[i] = count;
         }
         return arr;
@@ -24,6 +24,7 @@ public class NumberOfVisiblePeople {
 
     public static void main(String[] args) {
         int[][] tests = {
+                { 6, 2, 5, 4, 5, 1, 6 },
                 { 11, 19, 12, 15, 14, 18, 7, 1, 8, 9 },
                 { 10, 6, 8, 5, 11, 9 },
                 { 5, 1, 2, 3, 10 },
