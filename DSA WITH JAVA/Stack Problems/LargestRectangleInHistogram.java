7aacd1ce730cb8d8842d0d7ccfb5e205c85e5431
Chandan Saha <chandansaha1945@gmail.com>
2025-12-22 23:33:36 +0530

    LargestRectangleInHistogram

--- DSA WITH JAVA/Stack Problems/LargestRectangleInHistogram.java
+++ DSA WITH JAVA/Stack Problems/LargestRectangleInHistogram.java
@@ -1,16 +1,44 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 public class LargestRectangleInHistogram {
-    public static int largestRectangleArea(int[] heights) {
-        // placeholder
-        return 0;
+    public static int largestRectangleArea(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] nse = new int[n];
+        nse[n - 1] = -1;
+        st.push(arr[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() >= arr[i])
+                st.pop();
+            nse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(arr[i]);
+        }
+
+        while (!st.isEmpty())
+            st.pop();
+
+        int[] pse = new int[n];
+        pse[n - 1] = -1;
+        st.push(arr[0]);
+        for (int i = 1; i < n; i++) {
+            while (!st.isEmpty() && st.peek() >= arr[i])
+                st.pop();
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(arr[i]);
+        }
+        int maxArea = 0;
+        for (int i = 0; i < pse.length; i++) {
+            maxArea = Math.max(maxArea, arr[i] * (nse[i] - pse[i] - 1));
+        }
+        return maxArea;
     }
 
     public static void main(String[] args) {
         int[][] tests = {
-            {2, 1, 5, 6, 2, 3},
-            {2, 4},
-            {6, 2, 5, 4, 5, 1, 6}
+                { 2, 1, 5, 6, 2, 3 },
+                { 2, 4 },
+                { 6, 2, 5, 4, 5, 1, 6 }
         };
 
         for (int[] test : tests) {
