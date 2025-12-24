43601ab4be683418203a584b270d47da696acd9c
Chandan Saha <chandansaha1945@gmail.com>
2025-12-24 23:04:53 +0530

    PrimeFactors

--- DSA WITH JAVA/Stack Problems/PrimeFactors.java
+++ DSA WITH JAVA/Stack Problems/PrimeFactors.java
@@ -6,15 +6,12 @@ public class PrimeFactors {
         Stack<Integer> st = new Stack<>();
         System.out.print("Enter A Number = ");
         int n = sc.nextInt();
-        for (int i = 2; n > 1;) {
+        for (int i = 2; n > 1;)
             if (n % i == 0) {
                 n /= i;
                 st.push(i);
-            } else
-                i++;
-        }
-        while (!st.isEmpty())
-            System.out.print(st.pop() + " ");
+            } else i++;
+        while (!st.isEmpty()) System.out.print(st.pop() + " ");
         sc.close();
     }
 }
