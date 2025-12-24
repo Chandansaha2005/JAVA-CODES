a67f82e75a87aa4fc40c9359406fc79d1b3defbb
Chandan Saha <chandansaha1945@gmail.com>
2025-12-24 12:56:17 +0530

    MaximalRectangleInMatrix

--- DSA WITH JAVA/Stack Problems/MaximalRectangleInMatrix.java
+++ DSA WITH JAVA/Stack Problems/MaximalRectangleInMatrix.java
@@ -1,7 +1,59 @@
-class MaximalRectangleInMatrix{
+import java.util.Stack;
+
+class MaximalRectangleInMatrix {
     public static int maximalRectangle(char[][] matrix) {
-        // TODO: implement logic
+        int[][] mat = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                mat[i][j] = matrix[i][j] - '0';
+            }
+        }
+        // prefix sum Column wise. And if zero occur then continue or ignore it.
+        for (int i = 0; i < mat.length - 1; i++) {
+            if (mat[i][i + 1] == 0)
+                continue;
+            else
+                mat[i][i + 1] += mat[i][i];
+        }
+        for (int[] cs : mat) {
+            for (int c : cs) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
         return 0; // placeholder
+    }
+
+    public static int largestRectangleArea(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] nse = new int[n];
+        nse[n - 1] = n;
+        st.push(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i])
+                st.pop();
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        while (!st.isEmpty())
+            st.pop();
+
+        int[] pse = new int[n];
+        pse[0] = -1;
+        st.push(0);
+        for (int i = 1; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i])
+                st.pop();
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < pse.length; i++)
+            maxArea = Math.max(maxArea, arr[i] * (nse[i] - pse[i] - 1));
+        return maxArea;
     }
 
     public static void main(String[] args) {
