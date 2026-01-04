641f06cc929c0942a7e6dd68a6e606cc23a27c03
Chandan Saha <chandansaha1945@gmail.com>
2026-01-04 12:32:24 +0530

    display

--- DSA WITH JAVA/Queue/OperationsMethods.java
+++ DSA WITH JAVA/Queue/OperationsMethods.java
@@ -1,3 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 public class OperationsMethods {
-    
+    private static void display(Queue<Integer> q){
+        for (int i = 0; i < q.size(); i++) {
+            System.out.print(q.peek() + " ");
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+         Queue<Integer> q = new LinkedList<>();
+         q.add(10);
+         q.add(20);
+         q.add(30);
+         q.add(40);
+    }
 }
