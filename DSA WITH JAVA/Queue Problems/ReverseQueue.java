f4e8972fb9ef333990f88a653de577a6832a975f
Chandan Saha <chandansaha1945@gmail.com>
2026-01-04 15:48:56 +0530

    ReverseQueue gfg

--- DSA WITH JAVA/Queue Problems/ReverseQueue.java
+++ DSA WITH JAVA/Queue Problems/ReverseQueue.java
@@ -1,9 +1,14 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class ReverseQueue {
     public static void reverseQueue(Queue<Integer> q) {
-        
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty())
+            s.push(q.remove());
+        while (!s.isEmpty())
+            q.add(s.pop());
     }
 
     public static void main(String[] args) {
