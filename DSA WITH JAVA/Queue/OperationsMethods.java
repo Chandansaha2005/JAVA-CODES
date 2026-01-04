499ed92bdcdd68b7181796004ddaa24e5a1e7a57
Chandan Saha <chandansaha1945@gmail.com>
2026-01-04 12:35:46 +0530

    del

--- DSA WITH JAVA/Queue/OperationsMethods.java
+++ DSA WITH JAVA/Queue/OperationsMethods.java
@@ -1,19 +1,27 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 public class OperationsMethods {
-    private static void display(Queue<Integer> q){
+    private static void display(Queue<Integer> q) {
         for (int i = 0; i < q.size(); i++) {
             System.out.print(q.peek() + " ");
             q.add(q.remove());
         }
     }
+
+    private static int del(Queue<Integer> q) {
+        System.out.print("Delete = " + q.peek() + " ");
+        return q.remove();
+    }
+
     public static void main(String[] args) {
-         Queue<Integer> q = new LinkedList<>();
-         q.add(10);
-         q.add(20);
-         q.add(30);
-         q.add(40);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+
+        display(q);
+        del(q);
     }
 }
