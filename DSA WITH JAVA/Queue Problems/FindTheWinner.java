ff666aaa40c798bdea7a90841b6c5b28ffa4b951
Chandan Saha <chandansaha1945@gmail.com>
2026-01-05 22:41:37 +0530

    Remove commented sample test case descriptions in FindTheWinner.java

--- DSA WITH JAVA/Queue Problems/FindTheWinner.java
+++ DSA WITH JAVA/Queue Problems/FindTheWinner.java
@@ -16,15 +16,12 @@ class FindTheWinner {
     }
 
     public static void main(String[] args) {
-        // Sample test case 1
         int n1 = 5, k1 = 2;
         System.out.println("Test1 Output: " + findTheWinner(n1, k1));
 
-        // Sample test case 2
         int n2 = 6, k2 = 5;
         System.out.println("Test2 Output: " + findTheWinner(n2, k2));
 
-        // Sample test case 3 (edge case)
         int n3 = 1, k3 = 1;
         System.out.println("Test3 Output: " + findTheWinner(n3, k3));
     }
